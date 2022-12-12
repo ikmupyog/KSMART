0feaeb190b91baf601aa4577df98e78b48d1a93e
vbvarsha <varsha.ikm@kerala.gov.in>
2022-12-12 13:49:42 +0530

    user service

--- municipal-services/egov-filemgmnt/src/main/java/org/egov/filemgmnt/service/UserService.java
+++ municipal-services/egov-filemgmnt/src/main/java/org/egov/filemgmnt/service/UserService.java
@@ -1,13 +1,21 @@
 package org.egov.filemgmnt.service;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.LinkedHashMap;
 import java.util.List;
 
 import org.egov.common.contract.request.RequestInfo;
+import org.egov.common.contract.request.Role;
 import org.egov.filemgmnt.config.FMConfiguration;
 import org.egov.filemgmnt.repository.ServiceRequestRepository;
 import org.egov.filemgmnt.util.FMConstants;
 import org.egov.filemgmnt.web.models.ApplicantPersonal;
 import org.egov.filemgmnt.web.models.ApplicantPersonalRequest;
+import org.egov.filemgmnt.web.models.user.UserDetailResponse;
+import org.egov.filemgmnt.web.models.user.UserSearchRequest;
+import org.egov.tracer.model.CustomException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -16,29 +24,119 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 @Component
 class UserService {
 
-    private final ServiceRequestRepository restRepo;
-    private final FMConfiguration fmConfig;
-    private final ObjectMapper mapper;
+	private final ServiceRequestRepository restRepo;
+	private final FMConfiguration fmConfig;
+	private final ObjectMapper mapper;
 
-    @Autowired
-    UserService(ServiceRequestRepository restRepo, FMConfiguration fmConfig, ObjectMapper mapper) {
-        this.restRepo = restRepo;
-        this.fmConfig = fmConfig;
-        this.mapper = mapper;
+	@Autowired
+	UserService(ServiceRequestRepository restRepo, FMConfiguration fmConfig, ObjectMapper mapper) {
+		this.restRepo = restRepo;
+		this.fmConfig = fmConfig;
+		this.mapper = mapper;
 
-    }
+	}
 
-    public void createUser(ApplicantPersonalRequest request) {
-        RequestInfo requestInfo = request.getRequestInfo();
-        List<ApplicantPersonal> applicants = request.getApplicantPersonals();
+	public void createUser(ApplicantPersonalRequest request) {
 
-        final String serviceCode = FMConstants.BUSINESS_SERVICE_FM;
-        applicants.forEach(applicant -> {
-            // TODO: verify all fields required to create user in user service is available
-            // in applicantpersonal
+		List<ApplicantPersonal> applicant = request.getApplicantPersonals();
+		RequestInfo requestInfo = request.getRequestInfo();
+		Role role = getCitizenRole(applicant.get(0).getTenantId());
 
-            // create user
-        });
+		final String serviceCode = FMConstants.BUSINESS_SERVICE_FM;
+		applicant.forEach(personal -> {
 
-    }
+			String businessService = personal.getFileDetail().getBusinessService();
+		});
+
+	}
+
+	/**
+	 * Creates citizen role
+	 * 
+	 * @return Role object for citizen
+	 */
+	private Role getCitizenRole(String tenantId) {
+		Role role = new Role();
+		role.setCode("CITIZEN");
+		role.setName("Citizen");
+		role.setTenantId(getStateLevelTenant(tenantId));
+		return role;
+	}
+
+	private String getStateLevelTenant(String tenantId) {
+		return tenantId.split("\\.")[0];
+	}
+
+	private UserDetailResponse searchByUserName(String userName, String tenantId) {
+		UserSearchRequest userSearchRequest = new UserSearchRequest();
+		userSearchRequest.setUserType("CITIZEN");
+		userSearchRequest.setUserName(userName);
+		userSearchRequest.setTenantId(tenantId);
+		StringBuilder uri = new StringBuilder(fmConfig.getUserHost()).append(fmConfig.getUserSearchEndpoint());
+		return userCall(userSearchRequest, uri);
+
+	}
+
+	/**
+	 * Returns UserDetailResponse by calling user service with given uri and object
+	 * 
+	 * @param userRequest Request object for user service
+	 * @param uri         The address of the endpoint
+	 * @return Response from user service as parsed as userDetailResponse
+	 */
+	private UserDetailResponse userCall(Object userRequest, StringBuilder uri) {
+		String dobFormat = null;
+		if (uri.toString().contains(fmConfig.getUserSearchEndpoint())
+				|| uri.toString().contains(fmConfig.getUserUpdateEndpoint()))
+			dobFormat = "yyyy-MM-dd";
+		else if (uri.toString().contains(fmConfig.getUserCreateEndpoint()))
+			dobFormat = "dd/MM/yyyy";
+		try {
+			LinkedHashMap responseMap = (LinkedHashMap) restRepo.fetchResult(uri, userRequest);
+			parseResponse(responseMap, dobFormat);
+			UserDetailResponse userDetailResponse = mapper.convertValue(responseMap, UserDetailResponse.class);
+			return userDetailResponse;
+		} catch (IllegalArgumentException e) {
+			throw new CustomException("IllegalArgumentException", "ObjectMapper not able to convertValue in userCall");
+		}
+	}
+
+	/**
+	 * Converts date to long
+	 * 
+	 * @param date   date to be parsed
+	 * @param format Format of the date
+	 * @return Long value of date
+	 */
+	private Long dateTolong(String date, String format) {
+		SimpleDateFormat f = new SimpleDateFormat(format);
+		Date d = null;
+		try {
+			d = f.parse(date);
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		return d.getTime();
+	}
+
+	/**
+	 * Parses date formats to long for all users in responseMap
+	 * 
+	 * @param responeMap LinkedHashMap got from user api response
+	 */
+	private void parseResponse(LinkedHashMap responeMap, String dobFormat) {
+		List<LinkedHashMap> users = (List<LinkedHashMap>) responeMap.get("user");
+		String format1 = "dd-MM-yyyy HH:mm:ss";
+		if (users != null) {
+			users.forEach(map -> {
+				map.put("createdDate", dateTolong((String) map.get("createdDate"), format1));
+				if ((String) map.get("lastModifiedDate") != null)
+					map.put("lastModifiedDate", dateTolong((String) map.get("lastModifiedDate"), format1));
+				if ((String) map.get("dob") != null)
+					map.put("dob", dateTolong((String) map.get("dob"), dobFormat));
+				if ((String) map.get("pwdExpiryDate") != null)
+					map.put("pwdExpiryDate", dateTolong((String) map.get("pwdExpiryDate"), format1));
+			});
+		}
+	}
 }
---
+++ municipal-services/egov-filemgmnt/src/main/java/org/egov/filemgmnt/web/models/user/UserDetailResponse.java
@@ -0,0 +1,24 @@
+package org.egov.filemgmnt.web.models.user;
+
+import java.util.List;
+
+import org.egov.common.contract.response.ResponseInfo;
+import org.egov.filemgmnt.web.models.ApplicantPersonal;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class UserDetailResponse {
+
+	@JsonProperty("responseInfo")
+	ResponseInfo responseInfo;
+
+	@JsonProperty("user")
+	List<ApplicantPersonal> user;
+}
