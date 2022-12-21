8d5c31009ef144acabe05a5386869fc1e000f6c5
MayaDevops <unnimaya.ikm@kerala.gov.in>
2022-12-21 12:30:10 +0530

    CR

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/cr/src/pages/employee/Death-route/DeathCrFlow.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/cr/src/pages/employee/Death-route/DeathCrFlow.js
@@ -13,40 +13,47 @@ const DeathCrFlow = ({ path }) => {
       // img: <BankIcon />,
       link: `${path}/information-death`,
     },
-    {
-      title: "Name Inclusion",
-      subTitle: "Inbox",
-      // img: <FileProtected />,
-      link: `${path}/structure-type`,
-    },
+    // {
+    //   title: "Name Inclusion",
+    //   subTitle: "Inbox",
+    //   // img: <FileProtected />,
+    //   link: `${path}/structure-type`,
+    // },
     {
       title: "Correction",
       subTitle: "Inbox",
       // img: <FileProtected />, 
     },
-    {
-      title: "Suspension",
-      subTitle: "Inbox",
-      // img: <FileProtected />,
-    },
+    // {
+    //   title: "Suspension",
+    //   subTitle: "Inbox",
+    //   // img: <FileProtected />,
+    // },
     {
       title: "Cancellation",
       subTitle: "Inbox",
       // img: <FileProtected />,
     },
-    {
-      title: "Revoke",
-      subTitle: "Inbox",
-      // img: <FileProtected />,
-    },
+    // {
+    //   title: "Revoke",
+    //   subTitle: "Inbox",
+    //   // img: <FileProtected />,
+    // },
     
     
   ];
+  const ClassList = 
+  {
+   'New Registration':  'crfile',
+   'Correction':  'crfilecorrection', 
+   'Cancellation':  'crfileadoption', 
+  };
   return (
     <div>
       <div className="fileText">
         {" "}
-        <h3>Select Functional Modules</h3>
+        {/* <h3>Select Functional Modules</h3> */}
+        <CardLabel style={{fontSize: "15px",fontWeight: "400",marginBottom: "-18px"}}>{t("Select Functional Modules")}</CardLabel>
       </div>
       <div className="FileFlowWrapper">
         <div className="cardWrapper">
@@ -54,7 +61,7 @@ const DeathCrFlow = ({ path }) => {
            item.link?(
             <Link to={item.link}>
             {/* <Link to='trade-lisense'> */}
-            <div className="crfile">
+            <div className={ClassList[item.title]}>
               <div className="contentMenu">
                 <div className="contentImg">{item.img}</div>
                 <div className="contentText">
