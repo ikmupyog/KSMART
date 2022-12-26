8f9eaa777ce1320cf6636b3eb0a03bf32bf486c1
JasmineNazeer <115537272+JasmineNazeer@users.noreply.github.com>
2022-12-26 17:41:46 +0530

    New Migration script added

--- municipal-services/death-services/src/main/resources/db/migration/main/V20221210114700__crdeath_n_registry_ddl.sql
+++ municipal-services/death-services/src/main/resources/db/migration/main/V20221210114700__crdeath_n_registry_ddl.sql
@@ -1,4 +1,4 @@
-ALTER TABLE eg_death_dtls RENAME TO eg_death_dtls_old;
+--ALTER TABLE eg_death_dtls RENAME TO eg_death_dtls_old;
 
 DROP TABLE IF EXISTS public.eg_death_dtls;
 
@@ -95,7 +95,7 @@ CREATE TABLE IF NOT EXISTS public.eg_death_dtls
 );
 
 -- Table: public.eg_death_statistical_dtls
-ALTER TABLE eg_death_dtls RENAME TO eg_death_statistical_dtls_old;
+
 
 DROP TABLE IF EXISTS public.eg_death_statistical_dtls;
 
@@ -133,7 +133,7 @@ CREATE TABLE IF NOT EXISTS public.eg_death_statistical_dtls
 );
 
 -- Table: public.eg_death_address_dtls
-ALTER TABLE eg_death_dtls RENAME TO eg_death_address_dtls_old;
+
 
 DROP TABLE IF EXISTS public.eg_death_address_dtls;
 
@@ -179,7 +179,7 @@ CREATE TABLE IF NOT EXISTS public.eg_death_address_dtls
 
 -- Table public.eg_death_dtls_registry
 
-ALTER TABLE eg_death_dtls RENAME TO eg_death_dtls_registry_old;
+
 
 DROP TABLE IF EXISTS public.eg_death_dtls_registry;
 
@@ -272,7 +272,7 @@ CREATE TABLE IF NOT EXISTS public.eg_death_dtls_registry
 );
 
 -- Table: public.eg_death_statistical_registry
-ALTER TABLE eg_death_dtls RENAME TO eg_death_statistical_registry_old;
+
 
 DROP TABLE IF EXISTS public.eg_death_statistical_registry;
 
@@ -310,7 +310,7 @@ CREATE TABLE IF NOT EXISTS public.eg_death_statistical_registry
 );
 
 -- Table: public.eg_death_address_registry
-ALTER TABLE eg_death_dtls RENAME TO eg_death_address_registry_old;
+
 
 DROP TABLE IF EXISTS public.eg_death_address_registry;
 
