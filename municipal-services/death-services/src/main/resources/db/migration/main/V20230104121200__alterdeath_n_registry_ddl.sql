feee00da274d93136e235f62a14fab347d0b97e8
unknown <rakhis.ikm@kerala.gov.in>
2023-01-04 15:04:17 +0530

    migration script, create,log and registry n log

---
+++ municipal-services/death-services/src/main/resources/db/migration/main/V20230104121200__alterdeath_n_registry_ddl.sql
@@ -0,0 +1,13 @@
+ 
+ ALTER TABLE eg_death_dtls RENAME COLUMN deseased_passportno  TO deseased_idprooftype;  
+ ALTER TABLE eg_death_dtls ADD COLUMN deseased_idproofno character varying(64);
+ 
+ ALTER TABLE eg_death_dtls_log RENAME COLUMN deseased_passportno  TO deseased_idprooftype;  
+ ALTER TABLE eg_death_dtls_log ADD COLUMN deseased_idproofno character varying(64);
+ ALTER TABLE eg_death_dtls_log ADD COLUMN appl_type character varying(64);
+ 
+ ALTER TABLE eg_death_dtls_registry RENAME COLUMN deseased_passportno  TO deseased_idprooftype;  
+ ALTER TABLE eg_death_dtls_registry ADD COLUMN deseased_idproofno character varying(64);
+ 
+ ALTER TABLE eg_death_dtls_registry_log RENAME COLUMN deseased_passportno  TO deseased_idprooftype;  
+ ALTER TABLE eg_death_dtls_registry_log ADD COLUMN deseased_idproofno character varying(64);
