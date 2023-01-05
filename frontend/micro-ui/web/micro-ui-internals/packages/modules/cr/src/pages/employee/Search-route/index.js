cde020e4ce5176c9e1b5aff45a97cfb13c5e6d96
anzarthoghtripples <anzar@thoughtripples.com>
2023-01-05 13:56:06 +0530

    search

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/cr/src/pages/employee/Search-route/index.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/cr/src/pages/employee/Search-route/index.js
@@ -20,10 +20,7 @@ const SearchRoute = ({ parentUrl }) => {
         <PrivateRoute path={`${path}/birthsearch/:variant`} component={(props) => <Search {...props} parentRoute={path} />} />
         <PrivateRoute path={`${path}/deathsearch/:variant`} component={(props) => <Search {...props} parentRoute={path} />} />
         <PrivateRoute path={`${path}/search-correction/:variant`} component={(props) => <SearchCorrection {...props} parentRoute={path} />} />
-
         <PrivateRoute path={`${path}/application-details/:id`} component={() => <ApplicationDetails parentRoute={path} />} />
-
-    
       </Switch>
     </React.Fragment>
   );
