4aee4823c33f9416bd6e80ab1631f4c261f1fd7b
cinirs <cini.ikm@kerala.gov.in>
2023-01-06 14:27:23 +0530

    pde search controller

--- municipal-services/tl-services/src/main/java/org/egov/tl/web/controllers/TradeLicensePdeController.java
+++ municipal-services/tl-services/src/main/java/org/egov/tl/web/controllers/TradeLicensePdeController.java
@@ -28,103 +28,102 @@ import static org.egov.tl.util.TLConstants.businessService_TL;
 @RequestMapping("/pde")
 public class TradeLicensePdeController {
 
-    private final ObjectMapper objectMapper;
+        private final ObjectMapper objectMapper;
 
-    private final HttpServletRequest request;
+        private final HttpServletRequest request;
 
-    private final TradeLicensePdeService tradeLicensePdeService;
+        private final TradeLicensePdeService tradeLicensePdeService;
 
-    private final ResponseInfoFactory responseInfoFactory;
+        private final ResponseInfoFactory responseInfoFactory;
 
-    private final PaymentNotificationService paymentNotificationService;
+        private final PaymentNotificationService paymentNotificationService;
 
-    private final TLNotificationService tlNotificationService;
+        private final TLNotificationService tlNotificationService;
 
-    @Autowired
-    public TradeLicensePdeController(ObjectMapper objectMapper, HttpServletRequest request,
-            TradeLicensePdeService tradeLicensePdeService,
-            ResponseInfoFactory responseInfoFactory, PaymentNotificationService paymentNotificationService,
-            TLNotificationService tlNotificationService) {
-        this.objectMapper = objectMapper;
-        this.request = request;
-        this.tradeLicensePdeService = tradeLicensePdeService;
-        this.responseInfoFactory = responseInfoFactory;
-        this.paymentNotificationService = paymentNotificationService;
-        this.tlNotificationService = tlNotificationService;
-    }
+        @Autowired
+        public TradeLicensePdeController(ObjectMapper objectMapper, HttpServletRequest request,
+                        TradeLicensePdeService tradeLicensePdeService,
+                        ResponseInfoFactory responseInfoFactory, PaymentNotificationService paymentNotificationService,
+                        TLNotificationService tlNotificationService) {
+                this.objectMapper = objectMapper;
+                this.request = request;
+                this.tradeLicensePdeService = tradeLicensePdeService;
+                this.responseInfoFactory = responseInfoFactory;
+                this.paymentNotificationService = paymentNotificationService;
+                this.tlNotificationService = tlNotificationService;
+        }
 
-    @PostMapping({ "/{servicename}/_create", "/_create" })
-    public ResponseEntity<TradeLicenseResponse> create(@Valid @RequestBody TradeLicenseRequest tradeLicenseRequest,
-            @PathVariable(required = false) String servicename) {
-        List<TradeLicense> licenses = tradeLicensePdeService.create(tradeLicenseRequest, servicename);
-        TradeLicenseResponse response = TradeLicenseResponse.builder().licenses(licenses).responseInfo(
-                responseInfoFactory.createResponseInfoFromRequestInfo(tradeLicenseRequest.getRequestInfo(), true))
-                .build();
-        return new ResponseEntity<>(response, HttpStatus.OK);
-    }
+        @PostMapping({ "/{servicename}/_create", "/_create" })
+        public ResponseEntity<TradeLicenseResponse> create(@Valid @RequestBody TradeLicenseRequest tradeLicenseRequest,
+                        @PathVariable(required = false) String servicename) {
+                List<TradeLicense> licenses = tradeLicensePdeService.create(tradeLicenseRequest, servicename);
+                TradeLicenseResponse response = TradeLicenseResponse.builder().licenses(licenses).responseInfo(
+                                responseInfoFactory.createResponseInfoFromRequestInfo(
+                                                tradeLicenseRequest.getRequestInfo(), true))
+                                .build();
+                return new ResponseEntity<>(response, HttpStatus.OK);
+        }
 
-    // @RequestMapping(value = { "/{servicename}/_search", "/_search" }, method =
-    // RequestMethod.POST)
-    // public ResponseEntity<TradeLicenseResponse> search(@Valid @RequestBody
-    // RequestInfoWrapper requestInfoWrapper,
-    // @Valid @ModelAttribute TradeLicenseSearchCriteria criteria,
-    // @PathVariable(required = false) String servicename, @RequestHeader
-    // HttpHeaders headers) {
-    // List<TradeLicense> licenses = tradeLicensePdeService.search(criteria,
-    // requestInfoWrapper.getRequestInfo(),
-    // servicename, headers);
+        @RequestMapping(value = { "/{servicename}/_search", "/_search" }, method = RequestMethod.POST)
+        public ResponseEntity<TradeLicenseResponse> search(@Valid @RequestBody RequestInfoWrapper requestInfoWrapper,
+                        @Valid @ModelAttribute TradeLicenseSearchCriteria criteria,
+                        @PathVariable(required = false) String servicename, @RequestHeader HttpHeaders headers) {
+                List<TradeLicense> licenses = tradeLicensePdeService.search(criteria,
+                                requestInfoWrapper.getRequestInfo(),
+                                servicename, headers);
 
-    // int count = tradeLicensePdeService.countLicenses(criteria,
-    // requestInfoWrapper.getRequestInfo(), servicename,
-    // headers);
+                // int count = tradeLicensePdeService.countLicenses(criteria,
+                // requestInfoWrapper.getRequestInfo(), servicename,
+                // headers);
 
-    // TradeLicenseResponse response =
-    // TradeLicenseResponse.builder().licenses(licenses).responseInfo(
-    // responseInfoFactory.createResponseInfoFromRequestInfo(requestInfoWrapper.getRequestInfo(),
-    // true))
-    // .count(count)
-    // .build();
-    // return new ResponseEntity<>(response, HttpStatus.OK);
-    // }
+                TradeLicenseResponse response = TradeLicenseResponse.builder().licenses(licenses).responseInfo(
+                                responseInfoFactory.createResponseInfoFromRequestInfo(
+                                                requestInfoWrapper.getRequestInfo(),
+                                                true))
+                                .count(0)
+                                .build();
+                return new ResponseEntity<>(response, HttpStatus.OK);
+        }
 
-    @RequestMapping(value = { "/{servicename}/_update", "/_update" }, method = RequestMethod.POST)
-    public ResponseEntity<TradeLicenseResponse> update(@Valid @RequestBody TradeLicenseRequest tradeLicenseRequest,
-            @PathVariable(required = false) String servicename) {
-        List<TradeLicense> licenses = tradeLicensePdeService.update(tradeLicenseRequest, servicename);
+        @RequestMapping(value = { "/{servicename}/_update", "/_update" }, method = RequestMethod.POST)
+        public ResponseEntity<TradeLicenseResponse> update(@Valid @RequestBody TradeLicenseRequest tradeLicenseRequest,
+                        @PathVariable(required = false) String servicename) {
+                List<TradeLicense> licenses = tradeLicensePdeService.update(tradeLicenseRequest, servicename);
 
-        TradeLicenseResponse response = TradeLicenseResponse.builder().licenses(licenses).responseInfo(
-                responseInfoFactory.createResponseInfoFromRequestInfo(tradeLicenseRequest.getRequestInfo(),
-                        true))
-                .build();
-        return new ResponseEntity<>(response, HttpStatus.OK);
-    }
+                TradeLicenseResponse response = TradeLicenseResponse.builder().licenses(licenses).responseInfo(
+                                responseInfoFactory.createResponseInfoFromRequestInfo(
+                                                tradeLicenseRequest.getRequestInfo(),
+                                                true))
+                                .build();
+                return new ResponseEntity<>(response, HttpStatus.OK);
+        }
 
-    // @RequestMapping(value = { "/{servicename}/{jobname}/_batch", "/_batch" },
-    // method = RequestMethod.POST)
-    // public ResponseEntity sendReminderSMS(@Valid @RequestBody RequestInfoWrapper
-    // requestInfoWrapper,
-    // @PathVariable(required = false) String servicename,
-    // @PathVariable(required = true) String jobname) {
+        // @RequestMapping(value = { "/{servicename}/{jobname}/_batch", "/_batch" },
+        // method = RequestMethod.POST)
+        // public ResponseEntity sendReminderSMS(@Valid @RequestBody RequestInfoWrapper
+        // requestInfoWrapper,
+        // @PathVariable(required = false) String servicename,
+        // @PathVariable(required = true) String jobname) {
 
-    // tradeLicensePdeService.runJob(servicename, jobname,
-    // requestInfoWrapper.getRequestInfo());
+        // tradeLicensePdeService.runJob(servicename, jobname,
+        // requestInfoWrapper.getRequestInfo());
 
-    // return new ResponseEntity(HttpStatus.ACCEPTED);
-    // }
+        // return new ResponseEntity(HttpStatus.ACCEPTED);
+        // }
 
-    // @RequestMapping(value = "/_plainsearch", method = RequestMethod.POST)
-    // public ResponseEntity<TradeLicenseResponse> plainsearch(@Valid @RequestBody
-    // RequestInfoWrapper requestInfoWrapper,
-    // @Valid @ModelAttribute TradeLicenseSearchCriteria criteria) {
+        // @RequestMapping(value = "/_plainsearch", method = RequestMethod.POST)
+        // public ResponseEntity<TradeLicenseResponse> plainsearch(@Valid @RequestBody
+        // RequestInfoWrapper requestInfoWrapper,
+        // @Valid @ModelAttribute TradeLicenseSearchCriteria criteria) {
 
-    // List<TradeLicense> licenses = tradeLicensePdeService.plainSearch(criteria,
-    // requestInfoWrapper.getRequestInfo());
+        // List<TradeLicense> licenses = tradeLicensePdeService.plainSearch(criteria,
+        // requestInfoWrapper.getRequestInfo());
 
-    // TradeLicenseResponse response =
-    // TradeLicenseResponse.builder().licenses(licenses).responseInfo(
-    // responseInfoFactory.createResponseInfoFromRequestInfo(requestInfoWrapper.getRequestInfo(),
-    // true))
-    // .build();
-    // return new ResponseEntity<>(response, HttpStatus.OK);
-    // }
+        // TradeLicenseResponse response =
+        // TradeLicenseResponse.builder().licenses(licenses).responseInfo(
+        // responseInfoFactory.createResponseInfoFromRequestInfo(requestInfoWrapper.getRequestInfo(),
+        // true))
+        // .build();
+        // return new ResponseEntity<>(response, HttpStatus.OK);
+        // }
 }
