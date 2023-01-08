8f6aad62b68e5a633465eb7da9c9050d8fc33bbf
MayaDevops <unnimaya.ikm@kerala.gov.in>
2023-01-08 14:40:32 +0530

    cr

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/cr/src/pageComponents/birthComponents/HospitalDetails.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/cr/src/pageComponents/birthComponents/HospitalDetails.js
@@ -52,7 +52,7 @@ const HospitalDetails = ({ config, onSelect, userType, formData }) => {
     <React.Fragment>
       {/* {window.location.href.includes("/citizen") ? <Timeline currentStep={2} /> : null}
       {window.location.href.includes("/employee") ? <Timeline currentStep={2} /> : null} */}
-      <BackButton>{t("CS_COMMON_BACK")}</BackButton>
+      {/* <BackButton>{t("CS_COMMON_BACK")}</BackButton> */}
       <FormStep t={t} config={config} onSelect={goNext} onSkip={onSkip} isDisabled={!HospitalName}>
         <div className="row">
           <div className="col-md-12">
