f9181666b19d9583de960e5c30777d44993d153d
Jasmine <jasminenaz@gmail.com>
2023-01-13 17:38:17 +0530

    migration updated

--- municipal-services/death-services/src/main/resources/db/migration/main/V20230106102500__portingfields_death_ddl.sql
+++ municipal-services/death-services/src/main/resources/db/migration/main/V20230106102500__portingfields_death_ddl.sql
@@ -1,39 +1,39 @@
-DO $$
-BEGIN
-  IF EXISTS(SELECT * FROM information_schema.columns
-    WHERE table_name='eg_death_dtls' and column_name='correct_death_date_known')
-  THEN
-      ALTER TABLE "public"."eg_death_dtls" RENAME COLUMN "correct_death_date_known" TO "death_date_unavailable";
+-- DO $$
+-- BEGIN
+--   IF EXISTS(SELECT * FROM information_schema.columns
+--     WHERE table_name='eg_death_dtls' and column_name='correct_death_date_known')
+--   THEN
+    ALTER TABLE "public"."eg_death_dtls" RENAME COLUMN "correct_death_date_known" TO "death_date_unavailable";
 	  ALTER TABLE "public"."eg_death_dtls_log" RENAME COLUMN "correct_death_date_known" TO "death_date_unavailable";
 	  ALTER TABLE "public"."eg_death_dtls_registry" RENAME COLUMN "correct_death_date_known" TO "death_date_unavailable";
 	  ALTER TABLE "public"."eg_death_dtls_registry_log" RENAME COLUMN "correct_death_date_known" TO "death_date_unavailable";  
-  END IF;
-END $$;
+--   END IF;
+-- END $$;
 
-DO $$
-BEGIN
-  IF EXISTS(SELECT * FROM information_schema.columns
-    WHERE table_name='eg_death_dtls' and column_name='deceased_identified')
-  THEN
-      ALTER TABLE "public"."eg_death_dtls" RENAME COLUMN "deceased_identified" TO "deceased_unidentified";
+-- DO $$
+-- BEGIN
+--   IF EXISTS(SELECT * FROM information_schema.columns
+--     WHERE table_name='eg_death_dtls' and column_name='deceased_identified')
+--   THEN
+    ALTER TABLE "public"."eg_death_dtls" RENAME COLUMN "deceased_identified" TO "deceased_unidentified";
 	  ALTER TABLE "public"."eg_death_dtls_log" RENAME COLUMN "deceased_identified" TO "deceased_unidentified";
 	  ALTER TABLE "public"."eg_death_dtls_registry" RENAME COLUMN "deceased_identified" TO "deceased_unidentified";
 	  ALTER TABLE "public"."eg_death_dtls_registry_log" RENAME COLUMN "deceased_identified" TO "deceased_unidentified";
 	  
-  END IF;
-END $$;
+--   END IF;
+-- END $$;
 
-DO $$
-BEGIN
-  IF EXISTS(SELECT * FROM information_schema.columns
-    WHERE table_name='eg_death_dtls' and column_name='death_place_office_name')
-  THEN
+-- DO $$
+-- BEGIN
+  -- IF EXISTS(SELECT * FROM information_schema.columns
+  --   WHERE table_name='eg_death_dtls' and column_name='death_place_office_name')
+  -- THEN
       ALTER TABLE "public"."eg_death_dtls" RENAME COLUMN "death_place_office_name" TO "death_place_officer_name";
 	  ALTER TABLE "public"."eg_death_dtls_log" RENAME COLUMN "death_place_office_name" TO "death_place_officer_name";
 	  ALTER TABLE "public"."eg_death_dtls_registry" RENAME COLUMN "death_place_office_name" TO "death_place_officer_name";
 	  ALTER TABLE "public"."eg_death_dtls_registry_log" RENAME COLUMN "death_place_office_name" TO "death_place_officer_name";	  
-  END IF;
-END $$;
+--   END IF;
+-- END $$;
 
 ALTER TABLE eg_death_dtls_registry ADD COLUMN IF NOT EXISTS bad_record smallint;
 ALTER TABLE eg_death_dtls_registry ADD COLUMN IF NOT EXISTS record_no character varying(20);
