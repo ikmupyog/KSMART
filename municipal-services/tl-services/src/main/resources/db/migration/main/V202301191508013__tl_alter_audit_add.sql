952e75867ef9b6d387b26f6cf5a2cd9945ddcfcc
cinirs <cini.ikm@kerala.gov.in>
2023-01-19 17:11:17 +0530

    audit migration

---
+++ municipal-services/tl-services/src/main/resources/db/migration/main/V202301191508013__tl_alter_audit_add.sql
@@ -0,0 +1,16 @@
+ALTER TABLE eg_tl_tradelicensedetail_audit DROP COLUMN licenseunittype, DROP COLUMN licenseunitid,
+DROP COLUMN structureplacesubtype, DROP COLUMN customdetailtype,
+DROP COLUMN businessactivitydesc, DROP COLUMN licenseetype, DROP COLUMN institutionid;
+
+ALTER TABLE eg_tl_tradelicense_audit
+ADD COLUMN IF NOT EXISTS businesssector character varying(64),
+ADD COLUMN IF NOT EXISTS capitalinvestment numeric(14,2),
+ADD COLUMN IF NOT EXISTS enterprisetype character varying(64),
+ADD COLUMN IF NOT EXISTS licenseunittype character varying(64),
+ADD COLUMN IF NOT EXISTS licenseunitid character varying(64),
+ADD COLUMN IF NOT EXISTS structureplacesubtype character varying(64),
+ADD COLUMN IF NOT EXISTS customdetailtype character varying(300),
+ADD COLUMN IF NOT EXISTS businessactivitydesc character varying(1024),
+ADD COLUMN IF NOT EXISTS licenseetype character varying(64),
+ADD COLUMN IF NOT EXISTS mig_num_appln_id character varying(64),
+ADD COLUMN IF NOT EXISTS institutionid character varying(64);
