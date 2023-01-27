33170bde823a5fc9e0c7a2c0f8b86e449019e3d4
unknown <rakhis.ikm@kerala.gov.in>
2023-01-27 15:20:02 +0530

    migration

--- municipal-services/death-services/src/main/resources/db/migration/main/V20230111101000__burialdetails_death_ddl.sql
+++ municipal-services/death-services/src/main/resources/db/migration/main/V20230111101000__burialdetails_death_ddl.sql
@@ -33,15 +33,15 @@ ALTER TABLE eg_death_dtls_registry_log ADD COLUMN IF NOT EXISTS male_dependent_u
 ALTER TABLE eg_death_dtls_registry_log ADD COLUMN IF NOT EXISTS female_dependent_unavailable smallint;
 
 
-ALTER TABLE eg_death_statistical_dtls ADD COLUMN IF NOT EXISTS occupation_sub character varying(200);
-ALTER TABLE eg_death_statistical_dtls ADD COLUMN IF NOT EXISTS occupation_minor character varying(200);
-ALTER TABLE eg_death_statistical_dtls ADD COLUMN IF NOT EXISTS education_main character varying(200);
-ALTER TABLE eg_death_statistical_dtls ADD COLUMN IF NOT EXISTS  education_sub character varying(200);
+ALTER TABLE eg_death_statistical_dtls ADD COLUMN IF NOT EXISTS occupation_sub character varying(64);
+ALTER TABLE eg_death_statistical_dtls ADD COLUMN IF NOT EXISTS occupation_minor character varying(64);
+ALTER TABLE eg_death_statistical_dtls ADD COLUMN IF NOT EXISTS education_main character varying(64);
+ALTER TABLE eg_death_statistical_dtls ADD COLUMN IF NOT EXISTS  education_sub character varying(64);
 
-ALTER TABLE eg_death_statistical_dtls_log ADD COLUMN IF NOT EXISTS occupation_sub character varying(200);
-ALTER TABLE eg_death_statistical_dtls_log ADD COLUMN IF NOT EXISTS occupation_minor character varying(200);
-ALTER TABLE eg_death_statistical_dtls_log ADD COLUMN IF NOT EXISTS education_main character varying(200);
-ALTER TABLE eg_death_statistical_dtls_log ADD COLUMN IF NOT EXISTS  education_sub character varying(200);
+ALTER TABLE eg_death_statistical_dtls_log ADD COLUMN IF NOT EXISTS occupation_sub character varying(64);
+ALTER TABLE eg_death_statistical_dtls_log ADD COLUMN IF NOT EXISTS occupation_minor character varying(64);
+ALTER TABLE eg_death_statistical_dtls_log ADD COLUMN IF NOT EXISTS education_main character varying(64);
+ALTER TABLE eg_death_statistical_dtls_log ADD COLUMN IF NOT EXISTS  education_sub character varying(64);
 
 ALTER TABLE eg_death_applicant_dtls ADD COLUMN IF NOT EXISTS applicant_relation  character varying(64);
 
--- municipal-services/death-services/src/main/resources/db/migration/main/V20230112131900__statisticalregistrynewfields_ddl.sql
+++ municipal-services/death-services/src/main/resources/db/migration/main/V20230112131900__statisticalregistrynewfields_ddl.sql
@@ -1,12 +1,12 @@
-ALTER TABLE eg_death_statistical_registry ADD COLUMN IF NOT EXISTS occupation_sub character varying(200);
-ALTER TABLE eg_death_statistical_registry ADD COLUMN IF NOT EXISTS occupation_minor character varying(200);
-ALTER TABLE eg_death_statistical_registry ADD COLUMN IF NOT EXISTS education_main character varying(200);
-ALTER TABLE eg_death_statistical_registry ADD COLUMN IF NOT EXISTS  education_sub character varying(200);
+ALTER TABLE eg_death_statistical_registry ADD COLUMN IF NOT EXISTS occupation_sub character varying(64);
+ALTER TABLE eg_death_statistical_registry ADD COLUMN IF NOT EXISTS occupation_minor character varying(64);
+ALTER TABLE eg_death_statistical_registry ADD COLUMN IF NOT EXISTS education_main character varying(64);
+ALTER TABLE eg_death_statistical_registry ADD COLUMN IF NOT EXISTS  education_sub character varying(64);
 
-ALTER TABLE eg_death_statistical_registry_log ADD COLUMN IF NOT EXISTS occupation_sub character varying(200);
-ALTER TABLE eg_death_statistical_registry_log ADD COLUMN IF NOT EXISTS occupation_minor character varying(200);
-ALTER TABLE eg_death_statistical_registry_log ADD COLUMN IF NOT EXISTS education_main character varying(200);
-ALTER TABLE eg_death_statistical_registry_log ADD COLUMN IF NOT EXISTS  education_sub character varying(200);
+ALTER TABLE eg_death_statistical_registry_log ADD COLUMN IF NOT EXISTS occupation_sub character varying(64);
+ALTER TABLE eg_death_statistical_registry_log ADD COLUMN IF NOT EXISTS occupation_minor character varying(64);
+ALTER TABLE eg_death_statistical_registry_log ADD COLUMN IF NOT EXISTS education_main character varying(64);
+ALTER TABLE eg_death_statistical_registry_log ADD COLUMN IF NOT EXISTS  education_sub character varying(64);
 
 ALTER TABLE eg_death_statistical_registry ADD COLUMN IF NOT EXISTS  residenceLBType character varying(64);
 ALTER TABLE eg_death_statistical_registry_log ADD COLUMN IF NOT EXISTS  residenceLBType character varying(64);
