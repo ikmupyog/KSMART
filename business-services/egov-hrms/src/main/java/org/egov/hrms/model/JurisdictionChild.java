9f99e86084a32ac55cde786447c9728a0bd980d7
vbvarsha <varsha.ikm@kerala.gov.in>
2023-01-30 13:18:24 +0530

    notBlank in wardcode

--- business-services/egov-hrms/src/main/java/org/egov/hrms/model/JurisdictionChild.java
+++ business-services/egov-hrms/src/main/java/org/egov/hrms/model/JurisdictionChild.java
@@ -1,5 +1,7 @@
 package org.egov.hrms.model;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
@@ -42,6 +44,8 @@ public class JurisdictionChild {
 
 	@SafeHtml
 	@NotNull
+	@NotBlank
+	@NotEmpty
 	@Size(max = 256)
 	private String wardCode;
 
