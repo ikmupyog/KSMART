ae4e38450d7a3d0fd1d40aa91f494410874258fe
rajeshraghav <rajeshraghav.ikm@kerala.gov.in>
2023-02-09 16:43:20 +0530

    deletedfilerec

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/tl/src/Module.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/tl/src/Module.js
@@ -66,7 +66,6 @@ import SearchPde from "./pages/employee/SearchPde";
 import SearchPdeApplication from "./components/SearchPdeApplication";
 import TLLicenseUnitDet from "./pageComponents/TLLicenseUnitDet";
 import TLLicenseApplicantDet from "./pageComponents/TLLicenseApplicantDet";
-import TLDeclarationDet from "./pageComponents/TLDeclarationDet";
 import TLDocumentUpload from "./pageComponents/TLDocumentUpload";
 
 export const TLModule = ({ stateCode, userType, tenants }) => {
@@ -173,7 +172,6 @@ const componentsToRegister = {
   SearchPdeApplication,
   TLLicenseUnitDet,
   TLLicenseApplicantDet,
-  TLDeclarationDet,
   TLDocumentUpload
 };
 export const initTLComponents = () => {
