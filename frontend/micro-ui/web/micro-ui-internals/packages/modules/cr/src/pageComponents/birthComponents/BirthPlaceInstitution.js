bc48371d9af17af38fe914eacf741e0d9eb8ce3f
MayaDevops <unnimaya.ikm@kerala.gov.in>
2023-02-14 10:44:21 +0530

    birth

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/cr/src/pageComponents/birthComponents/BirthPlaceInstitution.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/cr/src/pageComponents/birthComponents/BirthPlaceInstitution.js
@@ -1,9 +1,9 @@
-import React, { useState } from "react";
-import { FormStep, CardLabel, Dropdown,Loader } from "@egovernments/digit-ui-react-components";
+import React, { useState, useEffect } from "react";
+import { FormStep, CardLabel, Dropdown, Loader } from "@egovernments/digit-ui-react-components";
 import { useTranslation } from "react-i18next";
 
-const BirthPlaceInstitution = ({ config, onSelect, userType, formData,institution ,setInstitution,institutionIdMl,setInstitutionIdMl,
-  institutionId,setInstitutionId
+const BirthPlaceInstitution = ({ config, onSelect, userType, formData, institution, setInstitution, institutionIdMl, setInstitutionIdMl,
+  institutionId, setInstitutionId
 }) => {
   const stateId = Digit.ULBService.getStateId();
   const { t } = useTranslation();
@@ -11,42 +11,53 @@ const BirthPlaceInstitution = ({ config, onSelect, userType, formData,institutio
   const { data: institutionList = {}, isinstitutionLoad } = Digit.Hooks.cr.useCivilRegistrationMDMS(stateId, "birth-death-service", "InstitutionType");
   // const { data: institutionidList = {}, isinstitutionidLoad } = Digit.Hooks.cr.useCivilRegistrationMDMS(stateId, "birth-death-service", "Institution");
   const { data: institutionidList = {}, isinstitutionidLoad } = Digit.Hooks.cr.useCivilRegistrationMDMS("kl.cochin", "cochin/egov-location", "institution");
-
+  const [isInitialRender, setIsInitialRender] = useState(true);
   // const [Institution, setInstitution] = useState(formData?.BirthPlaceInstitutionDetails?.Institution);
   // const [InstitutionIdMl, setInstitutionIdMl] = useState(formData?.BirthPlaceInstitutionDetails?.Institution);
   // const [InstitutionId, setInstitutionId] = useState(formData?.BirthPlaceInstitutionDetails?.InstitutionId);
-  let cmbInstitution = [];  
+  let cmbInstitution = [];
   institutionList &&
-  institutionList["birth-death-service"] &&
-  institutionList["birth-death-service"].InstitutionType.map((ob) => {
+    institutionList["birth-death-service"] &&
+    institutionList["birth-death-service"].InstitutionType.map((ob) => {
       cmbInstitution.push(ob);
     });
   ///institution-id
   let cmbInstitutionId = [];
+  let cmbInstitutionIdML = [];
   institutionidList &&
-  institutionidList["egov-location"] &&
-  institutionidList["egov-location"].institutionList.map((ob) => {
+    institutionidList["egov-location"] &&
+    institutionidList["egov-location"].institutionList.map((ob) => {
       cmbInstitutionId.push(ob);
     });
+  useEffect(() => {
 
+    if (isInitialRender) {
+      if (institutionId) {
+        cmbInstitutionIdML = cmbInstitutionId.filter((cmbInstitutionId) => cmbInstitutionId.institutionName === institutionId.institutionName);
+        setInstitutionIdMl(cmbInstitutionIdML[0]);
+        setIsInitialRender(false);
+      }
+    }
+  }, [cmbInstitutionIdML, isInitialRender])
   const onSkip = () => onSelect();
 
   function setselectInstitution(value) {
-   setInstitution(value);
+    setInstitution(value);
   }
   function setselectInstitutionId(value) {
     setInstitutionId(value);
+    setIsInitialRender(true);
   }
   function setselectInstitutionIdMl(value) {
     setInstitutionIdMl(value);
   }
 
- 
-  const goNext = () => {    
+
+  const goNext = () => {
     // console.log('clicked');
     // sessionStorage.setItem("Institution", Institution.code);
     // sessionStorage.setItem("InstitutionId", InstitutionId.code);
-   
+
     // onSelect(config.key, { 
     //   Institution, InstitutionId     
     // });
@@ -65,45 +76,46 @@ const BirthPlaceInstitution = ({ config, onSelect, userType, formData,institutio
           </div>
         </div>
         <div className="row">
-            <div className="col-md-4">
-              <CardLabel>{`${t("CR_INSTITUTION_TYPE")}`}<span className="mandatorycss">*</span></CardLabel>
-              <Dropdown
-                t={t}
-                optionKey="name"
-                isMandatory={true}
-                option={cmbInstitution}
-                selected={institution}
-                select={setselectInstitution}
-                placeholder={`${t("CR_INSTITUTION_TYPE")}`}
-              />
-            </div>
-            <div className="col-md-4">
-              <CardLabel>{`${t("CR_INSTITUTION_NAME_EN")}`}</CardLabel>
-              <Dropdown
-                t={t}
-                optionKey="institutionName"
-                isMandatory={true}
-                option={cmbInstitutionId}
-                selected={institutionId}
-                select={setselectInstitutionId}
-                placeholder={`${t("CR_INSTITUTION_NAME_EN")}`}
-              />
-            </div>
-            <div className="col-md-4">
-              <CardLabel>{`${t("CR_INSTITUTION_NAME_ML")}`}</CardLabel>
-              <Dropdown
-                t={t}
-                optionKey="institutionNamelocal"
-                isMandatory={true}
-                option={cmbInstitutionId}
-                selected={institutionIdMl}
-                select={setselectInstitutionIdMl}
-                placeholder={`${t("CR_INSTITUTION_NAME_ML")}`}
-              />
-            </div>
+          <div className="col-md-4">
+            <CardLabel>{`${t("CR_INSTITUTION_TYPE")}`}<span className="mandatorycss">*</span></CardLabel>
+            <Dropdown
+              t={t}
+              optionKey="name"
+              isMandatory={true}
+              option={cmbInstitution}
+              selected={institution}
+              select={setselectInstitution}
+              placeholder={`${t("CR_INSTITUTION_TYPE")}`}
+            />
+          </div>
+          <div className="col-md-4">
+            <CardLabel>{`${t("CR_INSTITUTION_NAME_EN")}`}</CardLabel>
+            <Dropdown
+              t={t}
+              optionKey="institutionName"
+              isMandatory={true}
+              option={cmbInstitutionId}
+              selected={institutionId}
+              select={setselectInstitutionId}
+              placeholder={`${t("CR_INSTITUTION_NAME_EN")}`}
+            />
+          </div>
+          <div className="col-md-4">
+            <CardLabel>{`${t("CR_INSTITUTION_NAME_ML")}`}</CardLabel>
+            <Dropdown
+              t={t}
+              optionKey="institutionNamelocal"
+              isMandatory={true}
+              option={cmbInstitutionId}
+              selected={institutionIdMl}
+              select={setselectInstitutionIdMl}
+              placeholder={`${t("CR_INSTITUTION_NAME_ML")}`}
+              disable={true}
+            />
+          </div>
         </div>
-        
-       
+
+
 
       </FormStep>
     </React.Fragment>
