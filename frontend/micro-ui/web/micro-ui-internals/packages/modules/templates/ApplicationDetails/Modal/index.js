9dddea02158135719ec729d450e6e6b8be8e201f
MayaDevops <unnimaya.ikm@kerala.gov.in>
2023-02-19 00:09:28 +0530

    AddressBirth

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/templates/ApplicationDetails/Modal/index.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/templates/ApplicationDetails/Modal/index.js
@@ -14,7 +14,7 @@ const ActionModal = (props) => {
   if (props?.businessService.includes("PT")) {
     return <PTActionModal {...props} />;
   }
-  if (props?.businessService.includes("NewBirth")) {
+  if (props?.businessService.includes("birth21days")) {
     return <BirthActionModal {...props} />;
   }
   if (props?.businessService.includes("NewBirthTwentyOne")) {
