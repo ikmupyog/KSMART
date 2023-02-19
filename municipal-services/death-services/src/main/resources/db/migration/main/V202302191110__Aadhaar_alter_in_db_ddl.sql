281a339587e70625d0fbe502185fc1444ef81605
unknown <rakhis.ikm@kerala.gov.in>
2023-02-19 11:13:38 +0530

    repository and migration script

---
+++ municipal-services/death-services/src/main/resources/db/migration/main/V202302191110__Aadhaar_alter_in_db_ddl.sql
@@ -0,0 +1,33 @@
+alter table eg_death_dtls alter column deceased_aadhar_number type character varying(500);
+alter table eg_death_dtls alter column informant_aadhar_no type character varying(500);
+alter table eg_death_dtls alter column male_dependent_aadharno type character varying(500);
+alter table eg_death_dtls alter column female_dependent_aadharno type character varying(500);
+alter table eg_death_dtls alter column death_place_officer_aadhaar type character varying(500);
+alter table eg_death_dtls alter column deceased_idproofno type character varying(500);
+alter table eg_death_dtls alter column spouse_aadhaar type character varying(500);
+alter table eg_death_dtls alter column initiator_aadhar_no type character varying(500);
+
+alter table eg_death_dtls_log alter column deceased_aadhar_number type character varying(500);
+alter table eg_death_dtls_log alter column informant_aadhar_no type character varying(500);
+alter table eg_death_dtls_log alter column male_dependent_aadharno type character varying(500);
+alter table eg_death_dtls_log alter column female_dependent_aadharno type character varying(500);
+alter table eg_death_dtls_log alter column death_place_officer_aadhaar type character varying(500);
+alter table eg_death_dtls_log alter column deceased_idproofno type character varying(500);
+alter table eg_death_dtls_log alter column spouse_aadhaar type character varying(500);
+alter table eg_death_dtls_log alter column initiator_aadhar_no type character varying(500);
+
+alter table eg_death_dtls_registry alter column deceased_aadhar_number type character varying(500);
+alter table eg_death_dtls_registry alter column informant_aadhar_no type character varying(500);
+alter table eg_death_dtls_registry alter column male_dependent_aadharno type character varying(500);
+alter table eg_death_dtls_registry alter column female_dependent_aadharno type character varying(500);
+alter table eg_death_dtls_registry alter column death_place_officer_aadhaar type character varying(500);
+alter table eg_death_dtls_registry alter column deceased_idproofno type character varying(500);
+alter table eg_death_dtls_registry alter column spouse_aadhaar type character varying(500);
+
+alter table eg_death_dtls_registry_log alter column deceased_aadhar_number type character varying(500);
+alter table eg_death_dtls_registry_log alter column informant_aadhar_no type character varying(500);
+alter table eg_death_dtls_registry_log alter column male_dependent_aadharno type character varying(500);
+alter table eg_death_dtls_registry_log alter column female_dependent_aadharno type character varying(500);
+alter table eg_death_dtls_registry_log alter column death_place_officer_aadhaar type character varying(500);
+alter table eg_death_dtls_registry_log alter column deceased_idproofno type character varying(500);
+alter table eg_death_dtls_registry_log alter column spouse_aadhaar type character varying(500);
