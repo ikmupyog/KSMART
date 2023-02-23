6b83aa55bbad62b075ea9268984298fba6c3372d
Nishad Ks <nishad@Nishads-MacBook-Air.local>
2023-02-23 12:32:15 +0530

    topnavbar label change reverted

--- frontend/micro-ui/web/micro-ui-internals/packages/react-components/src/atoms/TopBar.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/react-components/src/atoms/TopBar.js
@@ -3,33 +3,23 @@ import PropTypes from "prop-types";
 import Hamburger from "./Hamburger";
 import { NotificationBell } from "./svgindex";
 
-const TopBar = ({ img, isMobile, logoUrl, onLogout, toggleSidebar, ulb, userDetails, notificationCount, notificationCountLoaded, cityOfCitizenShownBesideLogo, onNotificationIconClick, hideNotificationIconOnSomeUrlsWhenNotLoggedIn,cityDetails,t }) => {
- let citizenHomeCity =Digit.SessionStorage.get("CITIZEN.COMMON.HOME.CITY" )
+const TopBar = ({ img, isMobile, logoUrl, onLogout, toggleSidebar, ulb, userDetails, notificationCount, notificationCountLoaded, cityOfCitizenShownBesideLogo, onNotificationIconClick, hideNotificationIconOnSomeUrlsWhenNotLoggedIn }) => {
   return (
     <div className="navbar">
       <div className="center-container">
         {isMobile && <Hamburger handleClick={toggleSidebar} />}
-        <img className="city" id="topbar-logo" src="https://s3.ap-south-1.amazonaws.com/ikm-egov-assets/logo-white.png" alt="K-SMART" />
-
-        {citizenHomeCity && (
-          <h3>
-            {t(citizenHomeCity?.name).toUpperCase()}
-          </h3>
-        )}
-      
-         {/* <h3>{cityOfCitizenShownBesideLogo}</h3> */}
-        
+        <img
+          className="city"
+          id="topbar-logo" 
+          src={img || "https://cdn.jsdelivr.net/npm/@egovernments/digit-ui-css@1.0.7/img/m_seva_white_logo.png"}
+          alt="mSeva"
+        />
+        <h3>{cityOfCitizenShownBesideLogo}</h3>
         <div className="RightMostTopBarOptions">
-          {!hideNotificationIconOnSomeUrlsWhenNotLoggedIn ? (
-            <div className="EventNotificationWrapper" onClick={onNotificationIconClick}>
-              {notificationCountLoaded && notificationCount ? (
-                <span>
-                  <p>{notificationCount}</p>
-                </span>
-              ) : null}
-              <NotificationBell />
-            </div>
-          ) : null}
+          {!hideNotificationIconOnSomeUrlsWhenNotLoggedIn ? <div className="EventNotificationWrapper" onClick={onNotificationIconClick}>
+            { notificationCountLoaded && notificationCount ? <span><p>{notificationCount}</p></span> : null }
+            <NotificationBell />
+          </div> : null}
         </div>
       </div>
     </div>
