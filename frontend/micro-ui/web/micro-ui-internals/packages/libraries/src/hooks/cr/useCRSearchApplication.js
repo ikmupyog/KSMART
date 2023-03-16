4327d2cbf480e7b06e8e5d1f1daf1fc32ad3a94f
lekshmybl <lekshmybl.ikm@kerala.gov.in>
2023-03-16 10:47:01 +0530

    search added

--- frontend/micro-ui/web/micro-ui-internals/packages/libraries/src/hooks/cr/useCRSearchApplication.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/libraries/src/hooks/cr/useCRSearchApplication.js
@@ -21,11 +21,12 @@ const combineResponse = (applications, workflowData, totalCount) => {
 const useCRSearch = (params, config) => {
   return async () => {
     const data = await Digit.CRService.search(params, config);
-    console.log("maya"+data);
-    const tenant = data?.Licenses?.[0]?.tenantId;
-    const businessIds = data?.Licenses.map((application) => application.applicationNumber);
+    console.log(data);
+    const tenant = data?.ChildDetails?.[0]?.tenantId;
+    console.log(data);
+    const businessIds = data?.ChildDetails.map((application) => application.applicationNumber);
     const workflowRes = await Digit.WorkflowService.getAllApplication(tenant, { businessIds: businessIds.join() });
-    return combineResponse(data?.Licenses, workflowRes?.ProcessInstances, data?.Count);
+    return combineResponse(data?.ChildDetails, workflowRes?.ProcessInstances, data?.Count);
   };
 };
 
@@ -35,21 +36,12 @@ export const useCRSearchApplication = (params, config = {}, t) => {
   const result = useQuery(["TL_APPLICATIONS_LIST", params], useCRSearch(params, config), {
     staleTime: Infinity,
     select: (data) => {
-      return data.map((i) => ({
+     return data.map((i) => ({
         TL_COMMON_TABLE_COL_APP_NO: i.applicationNumber,
-        TL_APPLICATION_CATEGORY: "ACTION_TEST_TRADE_LICENSE",
-        TL_COMMON_TABLE_COL_OWN_NAME: i?.tradeLicenseDetail?.subOwnerShipCategory.includes("INSTITUTION")
-          ? i?.tradeLicenseDetail?.institution?.name
-          : i?.tradeLicenseDetail?.owners?.map((ele, index) =>
-              index == 0 ? (multiownername = ele.name) : (multiownername = multiownername + " , " + ele.name)
-            ),
-        TL_COMMON_TABLE_COL_STATUS: `WF_NEWTL_${i?.status}`,
-        TL_COMMON_TABLE_COL_SLA_NAME: `${Math.round(i?.SLA / (1000 * 60 * 60 * 24))} ${t("TL_SLA_DAYS")}`,
-        TL_COMMON_TABLE_COL_TRD_NAME: i?.tradeName,
-        TL_INSTITUTION_TYPE_LABEL: i?.tradeLicenseDetail?.subOwnerShipCategory.includes("INSTITUTION")
-          ? `TL_${i?.tradeLicenseDetail?.subOwnerShipCategory}`
-          : null,
-        raw: i,
+        CR_FATHER_NAME: i.ParentsDetails?.fatherFirstNameEn,
+        CR_MOTHER_NAME: i.ParentsDetails?.motherFirstNameEn,
+        CR_ADDRESS:i.AddressBirthDetails?.houseNameNoEnPresent,
+        
       }));
     },
   });
@@ -65,7 +57,7 @@ export const useCRApplicationDetails = (params, config) => {
     //   return data.map(i => ({
     //     TL_COMMON_TABLE_COL_APP_NO: i.applicationNumber,
     //     TL_APPLICATION_CATEGORY: "ACTION_TEST_TRADE_LICENSE",
-    //     TL_COMMON_TABLE_COL_OWN_NAME: i?.tradeLicenseDetail?.owners?.map((ele) => ele?.name),
+    //     TL_COMMON_TABLE_COL_OWN_NAME: i?.ChildDetails?.owners?.map((ele) => ele?.name),
     //     TL_COMMON_TABLE_COL_STATUS: `WF_NEWTL_${i?.status}`,
     //     TL_COMMON_TABLE_COL_SLA_NAME: `${i?.SLA / (1000 * 60 * 60 * 24)} Days`,
     //     TL_COMMON_TABLE_COL_TRD_NAME: i?.tradeName,
