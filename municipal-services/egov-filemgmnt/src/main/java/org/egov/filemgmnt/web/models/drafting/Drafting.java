cc64a374ab32cdb596f0d0c93fef1760f2a8b44e
unknown <saraths.ikm@kerala.gov.in>
2023-03-24 15:06:16 +0530

    Drafting model class

---
+++ municipal-services/egov-filemgmnt/src/main/java/org/egov/filemgmnt/web/models/drafting/Drafting.java
@@ -0,0 +1,64 @@
+package org.egov.filemgmnt.web.models.drafting;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Positive;
+import javax.validation.constraints.Size;
+
+import org.springframework.validation.annotation.Validated;
+import org.egov.filemgmnt.web.models.AuditDetails;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Schema(description="A Object holds the file data of drafting submitted by the user")
+@Validated
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Drafting {
+
+    @Size(max=256)
+        @JsonProperty("uuid")
+        private String uuid;
+		
+		// drafting child table forgin key
+		@NotNull
+        @Size(max=256)
+        @JsonProperty("draftingChild")
+        private String draftingChild;
+	
+		@NotNull
+        @Size(max=256)
+        @JsonProperty("businessService")
+        private String businessService;
+		
+		@Schema(type = "string", description = "Workflow code")
+     // @NotBlank(message = "Workflow code is required")
+		@Size(max = 64, message = "Workflow code length cannot exceed 64 characters")
+		@JsonProperty("workflowCode")
+		private String workflowCode;
+		
+		@Schema(type = "string", description = "Draft Type")
+		@Size(max = 64, message = "Draft Type length cannot exceed 64 characters")
+		@JsonProperty("draftType")
+		private String draftType;
+		
+		@Size(max=64)
+        @JsonProperty("fileStoreId")
+        private String fileStoreId = null;
+		
+		@Schema(type = "string", description = "status")
+		@Size(max = 64, message = "status length cannot exceed 64 characters")
+		@JsonProperty("status")
+		private String status;
+    
+}
