b764b4f8dbf2f2c25a7475bbb60630ddf38c7296
Nishad Ks <nishad@Nishads-MacBook-Air.local>
2023-03-26 22:49:00 +0530

    adoption search apis

--- frontend/micro-ui/web/micro-ui-internals/packages/libraries/src/hooks/cr/useAdoptionSearch.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/libraries/src/hooks/cr/useAdoptionSearch.js
@@ -4,7 +4,7 @@ import { CRService } from "../../services/elements/CR"
 
 const useAdoptionSearch = ({tenantId, filters, config={}}) => useQuery(
     
-    ["CR_SEARCH", tenantId, ...Object.keys(filters)?.map( e => filters?.[e] )],
+    ["", tenantId, ...Object.keys(filters)?.map( e => filters?.[e] )],
     () => CRService.CRAdoptionSearch({tenantId, filters}),
     {
         // select: (data) => data.Licenses,
