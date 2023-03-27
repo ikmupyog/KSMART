524991f464d37878f1677ca4857645082e1ecea0
Neenuikm <neenurp.ikm@kerala.gov.in>
2023-03-27 16:51:50 +0530

    CREATE

--- municipal-services/marriage-services/src/main/resources/db/migration/main/V20230325112220__marriage_services_new_dll.sql
+++ municipal-services/marriage-services/src/main/resources/db/migration/main/V20230325112220__marriage_services_new_dll.sql
@@ -19,18 +19,18 @@ ALTER TABLE public.eg_marriage_present_address_details
 
 ALTER TABLE public.eg_marriage_present_address_details_audit
     ADD COLUMN IF NOT EXISTS poname_ml character varying(64) COLLATE pg_catalog."default";
-
-ALTER TABLE eg_marriage_bride_groom_details RENAME COLUMN firstname_mal TO firstname_ml;
-ALTER TABLE eg_marriage_bride_groom_details RENAME COLUMN middlename_mal TO middlename_ml;
-ALTER TABLE eg_marriage_bride_groom_details RENAME COLUMN mothername_mal TO mothername_ml;
-ALTER TABLE eg_marriage_bride_groom_details RENAME COLUMN guardianname_mal TO guardianname_ml;
-ALTER TABLE eg_marriage_bride_groom_details RENAME COLUMN lastname_mal TO lastname_ml;
-ALTER TABLE eg_marriage_bride_groom_details RENAME COLUMN fathername_mal TO fathername_ml;
-
-
-ALTER TABLE eg_marriage_bride_groom_details_audit RENAME COLUMN  firstname_mal TO firstname_ml;
-ALTER TABLE eg_marriage_bride_groom_details_audit RENAME COLUMN  middlename_mal TO middlename_ml;
-ALTER TABLE eg_marriage_bride_groom_details_audit RENAME COLUMN  mothername_mal TO mothername_ml;
-ALTER TABLE eg_marriage_bride_groom_details_audit RENAME COLUMN  guardianname_mal TO guardianname_ml;
-ALTER TABLE eg_marriage_bride_groom_details_audit RENAME COLUMN  lastname_mal TO lastname_ml;
-ALTER TABLE eg_marriage_bride_groom_details_audit RENAME COLUMN  fathername_mal TO fathername_ml;
+--
+--ALTER TABLE eg_marriage_bride_groom_details RENAME COLUMN firstname_mal TO firstname_ml;
+--ALTER TABLE eg_marriage_bride_groom_details RENAME COLUMN middlename_mal TO middlename_ml;
+--ALTER TABLE eg_marriage_bride_groom_details RENAME COLUMN mothername_mal TO mothername_ml;
+--ALTER TABLE eg_marriage_bride_groom_details RENAME COLUMN guardianname_mal TO guardianname_ml;
+--ALTER TABLE eg_marriage_bride_groom_details RENAME COLUMN lastname_mal TO lastname_ml;
+--ALTER TABLE eg_marriage_bride_groom_details RENAME COLUMN fathername_mal TO fathername_ml;
+--
+--
+--ALTER TABLE eg_marriage_bride_groom_details_audit RENAME COLUMN  firstname_mal TO firstname_ml;
+--ALTER TABLE eg_marriage_bride_groom_details_audit RENAME COLUMN  middlename_mal TO middlename_ml;
+--ALTER TABLE eg_marriage_bride_groom_details_audit RENAME COLUMN  mothername_mal TO mothername_ml;
+--ALTER TABLE eg_marriage_bride_groom_details_audit RENAME COLUMN  guardianname_mal TO guardianname_ml;
+--ALTER TABLE eg_marriage_bride_groom_details_audit RENAME COLUMN  lastname_mal TO lastname_ml;
+--ALTER TABLE eg_marriage_bride_groom_details_audit RENAME COLUMN  fathername_mal TO fathername_ml;
