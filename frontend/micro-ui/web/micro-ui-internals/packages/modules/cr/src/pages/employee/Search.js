de1d077fa25e965893fb34a9096c53cd8bdc4377
shruthy2022 <shruthyks007@gmail.com>
2023-03-30 12:18:32 +0530

    SBR

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/cr/src/pages/employee/Search.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/cr/src/pages/employee/Search.js
@@ -40,6 +40,10 @@ const Search = ({ path }) => {
         const { data: { ChildDetails: searchResult, Count: count } = {}, isLoading, isSuccess } = Digit.Hooks.cr.useAdoptionSearch({ tenantId, filters: payload, config })
         return <Search t={t} tenantId={tenantId} onSubmit={onSubmit} setApplicationType={setApplicationType} applicationType={applicationType} data={!isLoading && isSuccess ? (searchResult?.length > 0 ? searchResult : { display: "ES_COMMON_NO_DATA" }) : ""} count={count} />
     } 
+    else if(window.location.href.includes("/birthsearch") == true && applicationType?.value == "stillbirthsearch"){
+        const { data: { StillBirthChildDetails: searchResult, Count: count } = {}, isLoading, isSuccess } = Digit.Hooks.cr.useSearchStillBirth({ tenantId, filters: payload, config })
+        return <Search t={t} tenantId={tenantId} onSubmit={onSubmit} setApplicationType={setApplicationType} applicationType={applicationType} data={!isLoading && isSuccess ? (searchResult?.length > 0 ? searchResult : { display: "ES_COMMON_NO_DATA" }) : ""} count={count} />
+    } 
     else if(window.location.href.includes("/deathsearch") == true ) {
         const { data: { deathCertificateDtls: searchResult, Count: count } = {}, isLoading, isSuccess } = Digit.Hooks.cr.useSearchDeath({ tenantId, filters: payload, config })
 
