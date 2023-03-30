3abf926041d6c2ee8e96b5fecdcf4e7ae117ea4c
MayaDevops <unnimaya.ikm@kerala.gov.in>
2023-03-30 19:16:38 +0530

    bornoutsidebirth

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/core/src/pages/citizen/index.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/core/src/pages/citizen/index.js
@@ -35,7 +35,7 @@ const Home = ({
 }) => {
   const queryClient = useQueryClient();
   // queryClient.removeQueries("CR_CREATE_BIRTH_REG");  
-  sessionStorage.removeItem("Digit.CR_CREATE_BIRTH_REG");
+  // sessionStorage.removeItem("Digit.CR_CREATE_BIRTH_REG");
   sessionStorage.removeItem("Digit.CR_BIRTH_EDIT_FLAG");
   sessionStorage.removeItem("Digit.CR_EDIT_BIRTH_REG");
   const location = useLocation()
