9fdc20e59a0abab08cef47e7505c5475d4a9f9b6
Varun IKM <varun@ikm.gov.in>
2023-04-01 15:44:54 +0530

    Witness details Added

--- frontend/micro-ui/web/micro-ui-internals/packages/libraries/src/services/elements/MDMS.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/libraries/src/services/elements/MDMS.js
@@ -64,6 +64,21 @@ const initRequestBody = (tenantId) => ({
     ],
   },
 });
+const getMaritalStatusMasterList = (tenantId, moduleCode) => ({
+  details: {
+    tenantId: tenantId,
+    moduleDetails: [
+      {
+        moduleName: moduleCode,
+        masterDetails: [
+          {
+            name: "MaritalStatus",
+          },
+        ],
+      },
+    ],
+  },
+});
 const getWorkFlowBirthMasterList = (tenantId, moduleCode) => ({
   details: {
     tenantId: tenantId,
@@ -1037,8 +1052,7 @@ const getCROtherDeathPlacelist = (tenantId, moduleCode) => ({
     ],
   },
 });
-const getCRMannerOfDeathlist
- = (tenantId, moduleCode) => ({
+const getCRMannerOfDeathlist = (tenantId, moduleCode) => ({
   details: {
     tenantId: tenantId,
     moduleDetails: [
@@ -1053,8 +1067,7 @@ const getCRMannerOfDeathlist
     ],
   },
 });
-const getCRPregnantDeceasedlist
- = (tenantId, moduleCode) => ({
+const getCRPregnantDeceasedlist = (tenantId, moduleCode) => ({
   details: {
     tenantId: tenantId,
     moduleDetails: [
@@ -1069,8 +1082,7 @@ const getCRPregnantDeceasedlist
     ],
   },
 });
-const getCRRBirthStatuslist
- = (tenantId, moduleCode) => ({
+const getCRRBirthStatuslist = (tenantId, moduleCode) => ({
   details: {
     tenantId: tenantId,
     moduleDetails: [
@@ -1358,22 +1370,22 @@ const getCRMaleDependentlist = (tenantId, moduleCode) => ({
   },
 });
 const getBirthCorrectionDocumentslist = (tenantId, moduleCode) => {
-  return ({
-  details: {
-    tenantId: tenantId,
-    moduleDetails: [
-      {
-        moduleName: moduleCode,
-        masterDetails: [
-          {
-            name: "BirthCorrectionDocuments"
-          },
-        ],
-      },
-    ],
-  },
-});
-}
+  return {
+    details: {
+      tenantId: tenantId,
+      moduleDetails: [
+        {
+          moduleName: moduleCode,
+          masterDetails: [
+            {
+              name: "BirthCorrectionDocuments",
+            },
+          ],
+        },
+      ],
+    },
+  };
+};
 const getCRTitleList = (tenantId, moduleCode) => ({
   details: {
     tenantId: tenantId,
@@ -2331,6 +2343,9 @@ export const MdmsService = {
     // PersistantStorage.set(key, responseValue, cacheSetting.cacheTimeInSecs);
     return responseValue;
   },
+  getMaritalStatusMaster: (tenantId, moduleCode) => {
+    return MdmsService.getDataByCriteria(tenantId, getMaritalStatusMasterList(tenantId, moduleCode), moduleCode);
+  },
   getWorkFlowBirthMaster: (tenantId, moduleCode) => {
     return MdmsService.getDataByCriteria(tenantId, getWorkFlowBirthMasterList(tenantId, moduleCode), moduleCode);
   },
@@ -2415,7 +2430,7 @@ export const MdmsService = {
   getCRPregnantDeceased: (tenantId, moduleCode) => {
     return MdmsService.getDataByCriteria(tenantId, getCRPregnantDeceasedlist(tenantId, moduleCode), moduleCode);
   },
-  
+
   getCRBirthStatus: (tenantId, moduleCode) => {
     return MdmsService.getDataByCriteria(tenantId, getCRRBirthStatuslist(tenantId, moduleCode), moduleCode);
   },
@@ -2494,7 +2509,7 @@ export const MdmsService = {
   },
   getCRIdProofDetails: (tenantId, moduleCode) => {
     return MdmsService.getDataByCriteria(tenantId, getCRIdProofDetailsList(tenantId, moduleCode), moduleCode);
-  }, 
+  },
   getCRDeathPlaceType: (tenantId, moduleCode) => {
     return MdmsService.getDataByCriteria(tenantId, getCRDeathPlaceTypeList(tenantId, moduleCode), moduleCode);
   },
@@ -2569,7 +2584,6 @@ export const MdmsService = {
   GetTradeOwnerShipCategory: (tenantId, moduleCode, type) => {
     return MdmsService.getDataByCriteria(tenantId, getTradeOwnerShipCategoryCriteria(tenantId, moduleCode, type), moduleCode);
   },
-
 
   getPropertyOwnerType: (tenantId, moduleCode, type) => {
     return MdmsService.getDataByCriteria(tenantId, getPropertyOwnerTypeCriteria(tenantId, moduleCode, type), moduleCode);
