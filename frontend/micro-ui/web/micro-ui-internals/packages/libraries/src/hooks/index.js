2f1abd2a48ae8e620805c65585cfea1358efc6bf
MayaDevops <unnimaya.ikm@kerala.gov.in>
2023-04-03 09:53:08 +0530

    Address

--- frontend/micro-ui/web/micro-ui-internals/packages/libraries/src/hooks/index.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/libraries/src/hooks/index.js
@@ -127,6 +127,7 @@ import useCvilRegistrationAdoptionApi from "./cr/useCvilRegistrationAdoptionApi"
 import useCivilRegistrationStillBirthAPI from "./cr/useCivilRegistrationStillBirthAPI";
 import useCivilRegistrationNACBIRTHAPI from "./cr/useCivilRegistrationNACBIRTHAPI";
 import useCivilRegistrationBornOutsideIndiaBirthAPI from "./cr/useCivilRegistrationBornOutsideIndiaBirthAPI";
+import useCivilRegistrationAbandonedBirthAPI from "./cr/useCivilRegistrationAbandonedBirthAPI";
 import useApplicationBornOutsideIndiaBirthDetail from "./cr/useApplicationBornOutsideIndiaBirthDetail"; 
 
 import useCivilRegistrationDeathAPI from "./cr/useCivilRegistrationDeathAPI";
@@ -139,10 +140,12 @@ import useCRDFetchBill from "./cr/useDeathFetchBill";
 import useCRApplicationDetail from "./cr/useApplicationDetail";
 import useApplicationMarriageDetail from "./cr/useApplicationMarriageDetail";
 import useApplicationStillBirthDetail from "./cr/useApplicationStillBirthDetail"; 
+import useApplicationAbandondBirthDetail from "./cr/useApplicationAbandondBirthDetail";
 import useApplicationDeathDetail from "./cr/useApplicationDeathDetail"; 
 import useCRApplicationActions from "./cr/useApplicationActions";
 import useCRAdoptionApplicationActions from "./cr/useAdoptionApplActions";
 import useApplicationBirthNACActions from "./cr/useApplicationBirthNACActions";
+import useApplicationAbandonedBirthActions from "./cr/useApplicationAbandonedBirthActions";
 import useCRStillBirthApplicationActions from "./cr/useApplicationStillBirthActions";
 import useApplicationBornOutsideIndiaBirthActions from "./cr/useApplicationBornOutsideIndiaBirthActions";
 import useCRDeathApplicationActions from "./cr/useApplicationDeathActions";
@@ -150,6 +153,7 @@ import useCRGenderMDMS from "./cr/useCRGenderMDMS";
 import useCRInbox from "./cr/useInbox";
 import useCRMDMS from "./cr/useMDMS";
 import useCRSearch from "./cr/useSearch";
+import useAbandonedBirthSearch from "./cr/useAbandonedBirthSearch";
 import useBirthNACSearch from "./cr/useBirthNACSearch";
 import useAdoptionSearch from "./cr/useAdoptionSearch";
 import useRegistryDownloadDeath from "./cr/useRegistryDownloadDeath";
@@ -354,6 +358,7 @@ const cr = {
   useTenants: useTenantsCR,
   useCivilRegistrationMDMS,
   useCivilRegistrationAPI,
+  useCivilRegistrationAbandonedBirthAPI,
   useCivilRegistrationMarriageAPI,
   useCvilRegistrationAdoptionApi,
   useCivilRegistrationStillBirthAPI,
@@ -364,6 +369,7 @@ const cr = {
   useInbox: useCRInbox,
   useMDMS: useCRMDMS,
   useSearch: useCRSearch,
+  useAbandonedBirthSearch,
   useBirthNACSearch,
   useSearchMarriage,
   useAdoptionSearch,
@@ -377,11 +383,13 @@ const cr = {
   useApplicationDetail: useCRApplicationDetail,
   useApplicationMarriageDetail,
   useApplicationStillBirthDetail:useApplicationStillBirthDetail,
+  useApplicationAbandondBirthDetail,
   useApplicationBIRTHNACDetail,
   useApplicationBornOutsideIndiaBirthDetail,
   useApplicationActions: useCRApplicationActions,
   useAdoptionApplActions:useCRAdoptionApplicationActions,
   useCRStillBirthApplicationActions:useCRStillBirthApplicationActions,
+  useApplicationAbandonedBirthActions,
   useApplicationBirthNACActions,
   useApplicationBornOutsideIndiaBirthActions,
   useCRDeathApplicationActions: useCRDeathApplicationActions,
