848dd4001058d83767f3c1db686306b7a4d21ed5
MayaDevops <unnimaya.ikm@kerala.gov.in>
2023-04-03 09:59:19 +0530

    Birth NAC

--- frontend/micro-ui/web/micro-ui-internals/packages/libraries/src/services/atoms/urls.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/libraries/src/services/atoms/urls.js
@@ -131,6 +131,11 @@ const Urls = {
     search:"/birth-services/cr/searchbornoutside",
     update:"/birth-services/cr/updatebornoutside",  
   },
+  crabandonedbirth: {
+    create: "/birth-services/cr/createbornoutside",
+    search:"/birth-services/cr/searchbornoutside",
+    update:"/birth-services/cr/updatebornoutside",  
+  },
   crdeath: {
     create: "/death-services/v1/deathdetails/_createdeath ",
     search: "/death-services/v1/deathdetails/_searchdeath",
