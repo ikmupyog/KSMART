b6914dabae46be0a8cc168ba5cfa6a3ab794dd46
Neenuikm <neenurp.ikm@kerala.gov.in>
2023-04-12 14:41:46 +0530

    Document Search Added

--- municipal-services/marriage-services/src/main/java/org/ksmart/marriage/marriageapplication/repository/rowmapper/MarriageDocumentRowMapper.java
+++ municipal-services/marriage-services/src/main/java/org/ksmart/marriage/marriageapplication/repository/rowmapper/MarriageDocumentRowMapper.java
@@ -19,21 +19,10 @@ public class MarriageDocumentRowMapper implements ResultSetExtractor<List<Marria
         List<MarriageDocument> result = new ArrayList<>();
         while (rs.next()) {
             result.add(MarriageDocument.builder()
-                    .id(rs.getString("DOC_id"))
-                    .marriageTenantid(rs.getString("DOC_tenantid"))
-                    .documentName(rs.getString("DOC_document_name"))
-                    .documentType(rs.getString("DOC_document_type"))
-                    .fileStoreId(rs.getString("DOC_filestoreid"))
-                    .documentlink(rs.getString("DOC_document_link"))
-                    .marriageId(rs.getString("DOC_marriageid"))
-                    .documentOwner(rs.getString("DOC_bride_groom"))
-                    .active(rs.getBoolean("DOC_active"))
-                    .applicationNumber(rs.getString("DOC_applicationnumber"))
-                    .registrationNumber(rs.getString("DOC_registrationno"))
-                    .correctionId(rs.getString("DOC_correction_id"))
-                    .correctionFieldName(rs.getString("DOC_correction_field_name"))
-                    .applicationType(rs.getString("DOC_applicationtype"))
-                    .updatedFlag(rs.getInt("DOC_updated_flag"))
+                    .id(rs.getString("id"))
+                    .documentType(rs.getString("document_type"))
+                    .documentOwner(rs.getString("bride_groom"))
+                    .applicationNumber(rs.getString("applicationnumber"))
                     .marriageDocAuditDetails(getDocumentAuditDetails(rs))
                     .build());
         }
