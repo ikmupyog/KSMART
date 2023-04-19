42f53992e1f017b1138c1e9fb182c0cc2bb8b69e
JiffinVarghese1211 <jiffin@ikm.gov.in>
2023-04-19 20:16:59 +0530

    death correction

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/cr/src/components/DeathCorrectionModal.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/cr/src/components/DeathCorrectionModal.js
@@ -1,11 +1,11 @@
-import { DropIcon, EmployeeModuleCard, PopUp, EditButton, UploadFile } from "@egovernments/digit-ui-react-components";
+import { DropIcon, EmployeeModuleCard, PopUp, EditButton, UploadFile,Loader } from "@egovernments/digit-ui-react-components";
 import React, { useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
 import { checkForEmployee } from "../utils";
 import UploadDoc from "../../../../react-components/src/atoms/UploadDoc";
 import { DEATH_CORRECTION_FIELD_NAMES } from "../config/constants";
 
-const DeathCorrectionModal = ({ title, showModal, onSubmit, hideModal, selectedConfig,selectedDocs }) => {
+const DeathCorrectionModal = ({ title, showModal, onSubmit, hideModal, selectedConfig, selectedDocs, selectedDocData }) => {
   const { t } = useTranslation();
   let formData = {};
   let docIdDetails = [];
@@ -15,23 +15,30 @@ const DeathCorrectionModal = ({ title, showModal, onSubmit, hideModal, selectedC
   const [docuploadedId, setDocuploadedId] = useState();
   const [docuploadedName, setDocuploadedName] = useState();
   const [docuploadedType, setDocuploadedType] = useState();
+  const [fileDocError, setFileDocError] = useState();
   const [uploadedFile, setUploadedFile] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
   const [file, setFile] = useState(formData?.owners?.documents?.ProofOfIdentity);
   const [error, setError] = useState(null);
   let acceptFormat = ".jpg,.png,.pdf,.jpeg";
-  const handleUploadDoc = (file, docType) => {
-    let tempObj = { [docType]: [...file] };
-    setUploadDoc({ ...uploadDoc, ...tempObj });
-  };
 
-  useEffect(()=>{
-    return ()=>{
-      setUploadedFiles([]);
-    }
-  },[])
+
+  useEffect(() => {
+    if(selectedConfig?.Documents?.length > 0){
+    const existingDocIds = selectedConfig?.Documents?.map((item) => {
+      if (selectedDocs.includes(item.DocumentId?.toString())) {
+        return item.DocumentId?.toString();
+      }
+    });
+
+    const filteredData = selectedDocData.filter((item) => existingDocIds.includes(item.documentId));
+    console.log("initial value==",selectedConfig?.Documents,selectedDocData,selectedDocs,filteredData,existingDocIds);
+    setUploadedFiles([...filteredData]);
+  }
+  }, [selectedConfig?.Documents]);
 
   const resetFields = () => {
+    setFileDocError("");
     setUploadedFiles([]);
     setDocuploadedId("");
     setDocuploadedName("");
@@ -43,7 +50,7 @@ const DeathCorrectionModal = ({ title, showModal, onSubmit, hideModal, selectedC
     const removeindex = uploadedFiles.findIndex((element) => {
       return element.documentType === e;
     });
-   
+
     if (removeindex === -1) {
       return false;
     }
@@ -52,6 +59,7 @@ const DeathCorrectionModal = ({ title, showModal, onSubmit, hideModal, selectedC
   }
 
   function selectfile(e) {
+    setIsLoading(true);
     let result = selectedConfig?.Documents?.filter((obj) => obj.DocumentId == e?.target?.id);
     setDocuploadedName(result[0].DocumentList);
     setDocuploadedType(result[0].DocumentType);
@@ -60,15 +68,17 @@ const DeathCorrectionModal = ({ title, showModal, onSubmit, hideModal, selectedC
     setFile(e.target.files[0]);
   }
 
-
   useEffect(() => {
     (async () => {
-      setIsLoading(true);
-      setError(null);
+     
       if (file && file?.type) {
+       
+        setError(null);
         if (!acceptFormat?.split(",")?.includes(`.${file?.type?.split("/")?.pop()}`)) {
+          setIsLoading(false);
           setError(t("PT_UPLOAD_FORMAT_NOT_SUPPORTED"));
         } else if (file.size >= 2000000) {
+          setIsLoading(false);
           setError(t("PT_MAXIMUM_UPLOAD_SIZE_EXCEEDED"));
         } else {
           // try {
@@ -100,10 +110,10 @@ const DeathCorrectionModal = ({ title, showModal, onSubmit, hideModal, selectedC
             // formDetails?.[changeStatusIndex]?.isUploaded = false;
             // formDetails?.[changeStatusIndex]?.uploadedDocId = null;
             if (uploadedFiles?.findIndex((item) => item.documentType === temp.documentType) === -1) {
-              uploadedFiles.push(temp);
+              setUploadedFiles([...uploadedFiles, temp]);
             }
             setUploadedFile(response?.data?.files[0]?.fileStoreId);
-            setIsLoading(false);
+            await setIsLoading(false);
             // formDetails.isUploaded = true;
             // formDetails.uploadedDoc = response?.data?.files[0]?.fileStoreId
           } else {
@@ -116,6 +126,19 @@ const DeathCorrectionModal = ({ title, showModal, onSubmit, hideModal, selectedC
     })();
   }, [file, uploadedFiles]);
 
+ 
+  const renderLoader = (details) => {
+    if (isLoading && (details.DocumentId.toString() === docuploadedId)) {
+      return (
+        <div style={{margin:0}}>
+          <h1 style={{ fontWeight: "bold" }}>Uploading...</h1>
+        </div>
+      );
+    }
+  };
+
+  console.log("uploaded filess",uploadedFiles,selectedConfig?.Documents,docuploadedId);
+
   if (!showModal) {
     return null;
   }
@@ -126,6 +149,7 @@ const DeathCorrectionModal = ({ title, showModal, onSubmit, hideModal, selectedC
           <span style={{ background: "#fff", padding: "0 10px" }}>{`${fieldName} CHANGE`}</span>{" "}
         </h1>
         <h2 style={{ marginBottom: "1rem" }}>{`You have to upload the following documents to edit ${fieldName?.toLowerCase()}.`}</h2>
+        {fileDocError?.length > 0 && <p style={{ color: "red" }}>{fileDocError}</p>}
         {selectedConfig?.Documents?.map((item, index) => (
           <div>
             {!selectedDocs.includes(item.DocumentId.toString()) && (
@@ -146,7 +170,10 @@ const DeathCorrectionModal = ({ title, showModal, onSubmit, hideModal, selectedC
                     message={uploadedFile ? `1 ${t(`TL_ACTION_FILEUPLOADED`)}` : t(`TL_ACTION_NO_FILEUPLOADED`)}
                     error={error}
                   />
+               
                 </div>
+               
+                {renderLoader(item)}
               </div>
             )}
           </div>
@@ -156,8 +183,13 @@ const DeathCorrectionModal = ({ title, showModal, onSubmit, hideModal, selectedC
           selected={true}
           label={"Save"}
           onClick={() => {
-            resetFields();
-            onSubmit(uploadedFiles, error);
+            console.log("on save==", selectedConfig?.Documents, uploadedFiles);
+            if (!isLoading && (selectedConfig?.Documents?.length === uploadedFiles?.length)) {
+              resetFields();
+              onSubmit(uploadedFiles, error);
+            } else {
+              setFileDocError("You have to upload following documents to make changes in the field");
+            }
           }}
         />
         <EditButton
