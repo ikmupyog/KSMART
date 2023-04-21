edfa340bcfcce8f71cefc73679823ac285564158
binilp <binilp@ikm.gov.in>
2023-04-21 13:15:57 +0530

    changes in scripts

--- municipal-services/marriage-services/src/main/resources/db/migration/main/V20230421130601__addNewfieldInCertificateAuditTable_dll.sql
+++ municipal-services/marriage-services/src/main/resources/db/migration/main/V20230421130601__addNewfieldInCertificateAuditTable_dll.sql
@@ -1,3 +1,3 @@
-ALTER TABLE public.eg_marriage_certificate ADD COLUMN IF NOT EXISTS count int4;
-ALTER TABLE public.eg_marriage_certificate ALTER COLUMN registrationno TYPE varchar(64) USING registrationno::varchar;
+ALTER TABLE public.eg_marriage_certificate_audit ADD COLUMN IF NOT EXISTS count int4;
+ALTER TABLE public.eg_marriage_certificate_audit ALTER COLUMN registrationno TYPE varchar(64) USING registrationno::varchar;
 
