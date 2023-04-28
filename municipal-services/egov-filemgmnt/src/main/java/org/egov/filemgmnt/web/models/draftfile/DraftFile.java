3f13c4c7109649e25ae14a4b96042fc7a93cafe8
pribub <pribu.ikm@kerala.gov.in>
2023-04-28 11:36:45 +0530

    Draftfilemodel

--- municipal-services/egov-filemgmnt/src/main/java/org/egov/filemgmnt/web/models/draftfile/DraftFile.java
+++ municipal-services/egov-filemgmnt/src/main/java/org/egov/filemgmnt/web/models/draftfile/DraftFile.java
@@ -1,8 +1,11 @@
 package org.egov.filemgmnt.web.models.draftfile;
 
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
+import org.egov.filemgmnt.util.FMConstants;
 import org.egov.filemgmnt.web.models.AuditDetails;
 import org.springframework.validation.annotation.Validated;
 
@@ -15,7 +18,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@Schema(description = "A Object holds the file data of drafting submitted by the user")
+@Schema(description = "A Object holds the file data of draft file  submitted by the user")
 @Validated
 @Getter
 @Setter
@@ -25,49 +28,56 @@ import lombok.Setter;
 public class DraftFile {
 
     @Schema(type = "string", description = "Draft file id")
-    @Size(max = 256)
+    @Size(max = 64, message = "Draft file id length cannot exceed 64 characters")
     @JsonProperty("id")
     private String id;
 
     @Schema(type = "string", description = "Tenant identification number", example = "kl.cochin")
-    @Size(max = 64)
+    @NotBlank(message = "Tenant identification number is required")
+    @Size(max = 64, message = "Tenant identification number length cannot exceed 64 characters")
+    @Pattern(regexp = FMConstants.PATTERN_TENANT,
+            message = "Invalid tenant identification number format, ex: kl.cochin")
     @JsonProperty("tenantId")
     private String tenantId;
 
+    @Schema(type = "string", description = "Business service")
     @NotNull
-    @Size(max = 256)
+    @Size(max = 64, message = "Business service length cannot exceed 64 characters")
     @JsonProperty("businessService")
     private String businessService;
 
+    @Schema(type = "string", description = "Module name")
     @NotNull
-    @Size(max = 256)
+    @Size(max = 64,message = "Module name length cannot exceed 64 character")
     @JsonProperty("moduleName")
     private String moduleName;
 
+    @Schema(type = "string", description = "File code")
     @NotNull
-    @Size(max = 64)
+    @Size(max = 64, message = "File code length cannot exceed 64 character ")
     @JsonProperty("fileCode")
     private String fileCode;
 
     @NotNull
     @Schema(type = "string", description = "Draft Type")
-    @Size(max = 64, message = "Draft Type length cannot exceed 64 characters")
+    @Size(max = 64, message = "Draft type length cannot exceed 64 characters")
     @JsonProperty("draftType")
     private String draftType;
 
     @NotNull
     @Schema(type = "string", description = "Storing the Draft Content")
-    @Size(max = 1024)
+    @Size(max = 1024, message = "Draft text length cannot exceed 1024 characters")
     @JsonProperty("draftText")
     private String draftText;
 
     @NotNull
-    @Schema(type = "string", description = " draft created user")
-    @Size(max = 64)
+    @Schema(type = "string", description = " Assigner")
+    @Size(max = 64, message = "Assigner length cannot exceed 64 character")
     @JsonProperty("assigner")
     private String assigner;
 
-    @Size(max = 64)
+    @Schema(type = "string", description = " File store id")
+    @Size(max = 64,message = "File store id length cannot exceed 64 character")
     @JsonProperty("fileStoreId")
     private String fileStoreId;
 
