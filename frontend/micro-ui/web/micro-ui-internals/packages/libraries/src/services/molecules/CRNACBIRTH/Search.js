68d4bee49f3aa7c7ce4477c49a0c25690e307544
Anushaa2023 <anusha@ikm.gov.in>
2023-05-03 10:29:14 +0530

    NAC summary page

--- frontend/micro-ui/web/micro-ui-internals/packages/libraries/src/services/molecules/CRNACBIRTH/Search.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/libraries/src/services/molecules/CRNACBIRTH/Search.js
@@ -1,6 +1,6 @@
 import cloneDeep from "lodash/cloneDeep";
 import { CRNACBirthService } from "../../elements/CRNACBIRTH";
-// import { convertEpochToDateDMY } from  "../../utils";
+import { NA, getFormattedValue } from "../../../utils/dataFormatter";
 
 const stringReplaceAll = (str = "", searcher = "", replaceWith = "") => {
   if (searcher == "") return str;
@@ -35,21 +35,28 @@ const convertEpochToDate = (dateEpoch) => {
 export const CRNACsearch = {
   all: async (tenantId, filters = {}) => {
     const response = await CRNACBirthService.CRNACsearch({ tenantId, filters });
+    console.log(response, "response");
+
     return response;
   },
   application: async (tenantId, filters = {}) => {
     const response = await CRNACBirthService.CRNACsearch({ tenantId, filters });
+    console.log(response, "response");
+
     return response.nacDetails[0];
   },
 
   numberOfApplications: async (tenantId, filters = {}) => {
     const response = await CRNACBirthService.CRNACsearch({ tenantId, filters });
+    console.log(response, "response");
+
     return response.nacDetails[0];
   },
 
   applicationDetails: async (t, tenantId, applicationNumber, userType) => {
     const filter = { applicationNumber };
     const response = await CRNACsearch.application(tenantId, filter);
+    console.log(response, "response");
 
     let numOfApplications = [];
     if (response?.licenseNumber) {
@@ -93,103 +100,214 @@ export const CRNACsearch = {
         { title: "PDF_BIRTH_FATHER_AADHAR", value: response?.ParentsDetails?.fatherAadhar || "NA" },
       ],
     };
-    const PresentAddressBirthDetailsInfo = {
-      title: "CR_ADDRESS_INFORMATION_HEADER",
+    const AddressBirthDetailsPresentInfo = {
+      title: "CR_ADDRESS_DETAILS",
       values: [
         {
-          title: "PDF_BIRTH_PRESENT_ADDRESS",
+          title: "CS_COMMON_COUNTRY",
           value:
-            response?.AddressBirthDetails.presentInsideKeralaHouseNameEn +
-            " , " +
-            response?.AddressBirthDetails.presentInsideKeralaStreetNameEn +
-            " , " +
-            response?.AddressBirthDetails.presentInsideKeralaLocalityNameEn +
-            " , " +
-            response?.AddressBirthDetails.presentInsideKeralaVillage +
-            " , " +
-            response?.AddressBirthDetails.presentInsideKeralaTaluk +
-            " , " +
-            response?.AddressBirthDetails.presentInsideKeralaDistrict +
-            " , " +
-            response?.AddressBirthDetails.presentaddressStateName +
-            " , " +
-            response?.AddressBirthDetails.presentaddressCountry +
-            " , " +
-            response?.AddressBirthDetails.presentInsideKeralaPostOffice +
-            " , " +
-            response?.AddressBirthDetails.presentInsideKeralaPincode,
-        },
-
-        {
-          value:
-            response?.AddressBirthDetails.presentInsideKeralaHouseNameMl +
-              " , " +
-              response?.AddressBirthDetails.presentInsideKeralaStreetNameMl +
-              " , " +
-              response?.AddressBirthDetails.presentInsideKeralaLocalityNameMl +
-              " , " +
-              response?.AddressBirthDetails.presentInsideKeralaVillage +
-              " , " +
-              response?.AddressBirthDetails.presentInsideKeralaTaluk +
-              " , " +
-              response?.AddressBirthDetails.presentInsideKeralaDistrict +
-              " , " +
-              response?.AddressBirthDetails.presentaddressStateName +
-              " , " +
-              response?.AddressBirthDetails.presentaddressCountry +
-              " , " +
-              response?.AddressBirthDetails.presentInsideKeralaPostOffice +
-              " , " +
-              response?.AddressBirthDetails.presentInsideKeralaPincode || "CR_NOT_RECORDED",
-        },
-
-        {
-          title: "PDF_BIRTH_PERMANENT_ADDRESS",
-          value:
-            response?.AddressBirthDetails.permntInKeralaAdrHouseNameEn +
-            " , " +
-            response?.AddressBirthDetails.permntInKeralaAdrStreetNameEn +
-            " , " +
-            response?.AddressBirthDetails.permntInKeralaAdrLocalityNameEn +
-            " , " +
-            response?.AddressBirthDetails.permntInKeralaAdrVillage +
-            " , " +
-            response?.AddressBirthDetails.permntInKeralaAdrTaluk +
-            " , " +
-            response?.AddressBirthDetails.permntInKeralaAdrDistrict +
-            " , " +
-            response?.AddressBirthDetails.permtaddressStateName +
-            " , " +
-            response?.AddressBirthDetails.permtaddressCountry +
-            " , " +
-            response?.AddressBirthDetails.permntInKeralaAdrPostOffice +
-            " , " +
-            response?.AddressBirthDetails.permntInKeralaAdrPincode,
+            response?.AddressBirthDetails.countryIdPresentEn +
+              " / " +
+              (response?.AddressBirthDetails?.countryIdPresentMl != null ? response?.AddressBirthDetails?.countryIdPresentMl : "") || "NA",
         },
         {
-          value:
-            response?.AddressBirthDetails.permntInKeralaAdrHouseNameMl +
-            " , " +
-            response?.AddressBirthDetails.permntInKeralaAdrStreetNameMl +
-            " , " +
-            response?.AddressBirthDetails.permntInKeralaAdrLocalityNameMl +
-            " , " +
-            response?.AddressBirthDetails.permntInKeralaAdrVillage +
-            " , " +
-            response?.AddressBirthDetails.permntInKeralaAdrTaluk +
-            " , " +
-            response?.AddressBirthDetails.permntInKeralaAdrDistrict +
-            " , " +
-            response?.AddressBirthDetails.permtaddressStateName +
-            " , " +
-            response?.AddressBirthDetails.permtaddressCountry +
-            " , " +
-            response?.AddressBirthDetails.permntInKeralaAdrPostOffice +
-            " , " +
-            response?.AddressBirthDetails.permntInKeralaAdrPincode,
+          title: "CS_COMMON_STATE",
+          value: response?.AddressBirthDetails.stateIdPresentEn + " / " + response?.AddressBirthDetails.stateIdPresentMl || "NA",
         },
+        {
+          title: "CS_COMMON_DISTRICT",
+          value: response?.AddressBirthDetails?.districtIdPresentEn + " / " + response?.AddressBirthDetails.districtIdPresentMl || "NA",
+        },
+        { title: "CS_COMMON_TALUK", value: response?.AddressBirthDetails?.presentInsideKeralaTaluk || "NA" },
+        { title: "CS_COMMON_VILLAGE", value: response?.AddressBirthDetails?.presentInsideKeralaVillage || "NA" },
+        {
+          title: "CS_COMMON_LB_NAME",
+          value:
+            response?.AddressBirthDetails?.presentInsideKeralaLBNameEn + " / " + response?.AddressBirthDetails?.presentInsideKeralaLBNameMl || "NA",
+        },
+        { title: "CS_COMMON_WARD", value: response?.AddressBirthDetails?.presentWardNo || "NA" },
+        {
+          title: "CS_COMMON_POST_OFFICE",
+          value:
+            response?.AddressBirthDetails?.presentInsideKeralaPostOfficeEn + " / " + response?.AddressBirthDetails?.presentInsideKeralaPostOfficeMl ||
+            "NA",
+        },
+        { title: "CS_COMMON_PIN_CODE", value: response?.AddressBirthDetails.presentInsideKeralaPincode || "NA" },
+        { title: "CR_LOCALITY_EN", value: response?.AddressBirthDetails?.presentInsideKeralaLocalityNameEn || NA },
+        { title: "CR_LOCALITY_ML", value: response?.AddressBirthDetails?.presentInsideKeralaLocalityNameMl || "NA" },
+        { title: "CR_STREET_NAME_EN", value: response?.AddressBirthDetails?.presentInsideKeralaStreetNameEn || "NA" },
+        { title: "CR_STREET_NAME_ML", value: response?.AddressBirthDetails?.presentInsideKeralaStreetNameMl || "NA" },
+        { title: "CR_HOUSE_NAME_EN", value: response?.AddressBirthDetails?.presentInsideKeralaHouseNameEn || "NA" },
+        { title: "CR_HOUSE_NAME_ML", value: response?.AddressBirthDetails?.presentInsideKeralaHouseNameMl || "NA" },
+        //Permanent Address
+        {
+          title: "CS_COMMON_COUNTRY",
+          value:
+            response?.AddressBirthDetails.countryIdPermanentEn +
+              " / " +
+              (response?.ParentsDetails?.countryIdPermanentMl != null ? response?.ParentsDetails?.countryIdPermanentMl : "") || "NA",
+        },
+        {
+          title: "CS_COMMON_STATE",
+          value: response?.AddressBirthDetails.stateIdPermanentEn + " / " + response?.AddressBirthDetails.stateIdPermanentMl || "NA",
+        },
+        {
+          title: "CS_COMMON_DISTRICT",
+          value: response?.AddressBirthDetails?.districtIdPermanentEn + " / " + response?.AddressBirthDetails.districtIdPermanentMl || "NA",
+        },
+        { title: "CS_COMMON_TALUK", value: response?.AddressBirthDetails?.permntInKeralaAdrTaluk || "NA" },
+        { title: "CS_COMMON_VILLAGE", value: response?.AddressBirthDetails?.permntInKeralaAdrVillage || "NA" },
+        {
+          title: "CS_COMMON_LB_NAME",
+          value: response?.AddressBirthDetails?.permntInKeralaAdrLBNameEn + " / " + response?.AddressBirthDetails?.permntInKeralaAdrLBNameMl || "NA",
+        },
+        { title: "CS_COMMON_WARD", value: response?.AddressBirthDetails?.permntInKeralaWardNo || "NA" },
+        {
+          title: "CS_COMMON_POST_OFFICE",
+          value:
+            response?.AddressBirthDetails?.permntInKeralaAdrPostOfficeEn + " / " + response?.AddressBirthDetails?.permntInKeralaAdrPostOfficeMl ||
+            "NA",
+        },
+        { title: "CS_COMMON_PIN_CODE", value: response?.AddressBirthDetails.permntInKeralaAdrPincode || "NA" },
+        { title: "CR_LOCALITY_EN", value: response?.AddressBirthDetails?.permntInKeralaAdrLocalityNameEn || NA },
+        { title: "CR_LOCALITY_ML", value: response?.AddressBirthDetails?.permntInKeralaAdrLocalityNameMl || "NA" },
+        { title: "CR_STREET_NAME_EN", value: response?.AddressBirthDetails?.permntInKeralaAdrStreetNameEn || "NA" },
+        { title: "CR_STREET_NAME_ML", value: response?.AddressBirthDetails?.permntInKeralaAdrStreetNameMl || "NA" },
+        { title: "CR_HOUSE_NAME_EN", value: response?.AddressBirthDetails?.permntInKeralaAdrHouseNameEn || "NA" },
+        { title: "CR_HOUSE_NAME_ML", value: response?.AddressBirthDetails?.permntInKeralaAdrHouseNameMl || "NA" },
       ],
     };
+    const AddressBirthDetailsPresentOutsideKeralaInfo = {
+      title: "CR_ADDRESS_DETAILS",
+      values: [
+        { title: "CS_COMMON_COUNTRY", value: response?.AddressBirthDetails.presentaddressCountry || "NA" },
+        { title: "CS_COMMON_STATE", value: response?.AddressBirthDetails?.presentaddressStateName || "NA" },
+        { title: "CS_COMMON_DISTRICT", value: response?.AddressBirthDetails?.presentOutsideKeralaDistrict || "NA" },
+        { title: "CR_TALUK_TEHSIL", value: response?.AddressBirthDetails?.presentOutsideKeralaTaluk || "NA" },
+        { title: "CR_TOWN_VILLAGE_EN", value: response?.AddressBirthDetails?.presentOutsideKeralaVillage || "NA" },
+        { title: "CR_CITY_VILLAGE_NAME_EN", value: response?.AddressBirthDetails?.presentOutsideKeralaCityVilgeEn || "NA" },
+        { title: "CS_COMMON_POST_OFFICE", value: response?.AddressBirthDetails?.presentOutsideKeralaPostOfficeEn || "NA" },
+        { title: "CS_COMMON_PIN_CODE", value: response?.AddressBirthDetails?.presentOutsideKeralaPincode || "NA" },
+        { title: "CR_LOCALITY_EN", value: response?.AddressBirthDetails.presentOutsideKeralaLocalityNameEn || "NA" },
+        { title: "CR_LOCALITY_ML", value: response?.AddressBirthDetails?.presentOutsideKeralaLocalityNameMl || NA },
+        { title: "CR_STREET_NAME_EN", value: response?.AddressBirthDetails?.presentOutsideKeralaStreetNameEn || "NA" },
+        { title: "CR_STREET_NAME_ML", value: response?.AddressBirthDetails?.presentOutsideKeralaStreetNameMl || "NA" },
+        { title: "CR_HOUSE_NAME_EN", value: response?.AddressBirthDetails?.presentOutsideKeralaHouseNameEn || "NA" },
+        { title: "CR_HOUSE_NAME_ML", value: response?.AddressBirthDetails?.presentOutsideKeralaHouseNameMl || "NA" },
+
+        { title: "CS_COMMON_COUNTRY", value: response?.AddressBirthDetails.presentaddressCountry || "NA" },
+        { title: "CS_COMMON_STATE", value: response?.AddressBirthDetails?.presentaddressStateName || "NA" },
+        { title: "CS_COMMON_DISTRICT", value: response?.AddressBirthDetails?.permntOutsideKeralaDistrict || "NA" },
+        { title: "CR_TALUK_TEHSIL", value: response?.AddressBirthDetails?.permntOutsideKeralaTaluk || "NA" },
+        { title: "CR_TOWN_VILLAGE_EN", value: response?.AddressBirthDetails?.permntOutsideKeralaVillage || "NA" },
+        { title: "CR_CITY_VILLAGE_NAME_EN", value: response?.AddressBirthDetails?.permntOutsideKeralaCityVilgeEn || "NA" },
+        { title: "CS_COMMON_POST_OFFICE", value: response?.AddressBirthDetails?.permntOutsideKeralaPostOfficeEn || "NA" },
+        { title: "CS_COMMON_PIN_CODE", value: response?.AddressBirthDetails?.permntOutsideKeralaPincode || "NA" },
+        { title: "CR_LOCALITY_EN", value: response?.AddressBirthDetails.permntOutsideKeralaLocalityNameEn || "NA" },
+        { title: "CR_LOCALITY_ML", value: response?.AddressBirthDetails?.permntOutsideKeralaLocalityNameMl || NA },
+        { title: "CR_STREET_NAME_EN", value: response?.AddressBirthDetails?.permntOutsideKeralaStreetNameEn || "NA" },
+        { title: "CR_STREET_NAME_ML", value: response?.AddressBirthDetails?.permntOutsideKeralaStreetNameMl || "NA" },
+        { title: "CR_HOUSE_NAME_EN", value: response?.AddressBirthDetails?.permntOutsideKeralaHouseNameEn || "NA" },
+        { title: "CR_HOUSE_NAME_ML", value: response?.AddressBirthDetails?.permntOutsideKeralaHouseNameMl || "NA" },
+      ],
+    };
+    // const PresentAddressBirthDetailsInfo = {
+    //   title: "CR_ADDRESS_INFORMATION_HEADER",
+    //   values: [
+    //     {
+    //       title: "PDF_BIRTH_PRESENT_ADDRESS",
+    //       value:
+    //         response?.AddressBirthDetails.presentInsideKeralaHouseNameEn +
+    //         " , " +
+    //         response?.AddressBirthDetails.presentInsideKeralaStreetNameEn +
+    //         " , " +
+    //         response?.AddressBirthDetails.presentInsideKeralaLocalityNameEn +
+    //         " , " +
+    //         response?.AddressBirthDetails.presentInsideKeralaVillage +
+    //         " , " +
+    //         response?.AddressBirthDetails.presentInsideKeralaTaluk +
+    //         " , " +
+    //         response?.AddressBirthDetails.presentInsideKeralaDistrict +
+    //         " , " +
+    //         response?.AddressBirthDetails.presentaddressStateName +
+    //         " , " +
+    //         response?.AddressBirthDetails.presentaddressCountry +
+    //         " , " +
+    //         response?.AddressBirthDetails.presentInsideKeralaPostOffice +
+    //         " , " +
+    //         response?.AddressBirthDetails.presentInsideKeralaPincode,
+    //     },
+
+    //     {
+    //       value:
+    //         response?.AddressBirthDetails.presentInsideKeralaHouseNameMl +
+    //           " , " +
+    //           response?.AddressBirthDetails.presentInsideKeralaStreetNameMl +
+    //           " , " +
+    //           response?.AddressBirthDetails.presentInsideKeralaLocalityNameMl +
+    //           " , " +
+    //           response?.AddressBirthDetails.presentInsideKeralaVillage +
+    //           " , " +
+    //           response?.AddressBirthDetails.presentInsideKeralaTaluk +
+    //           " , " +
+    //           response?.AddressBirthDetails.presentInsideKeralaDistrict +
+    //           " , " +
+    //           response?.AddressBirthDetails.presentaddressStateName +
+    //           " , " +
+    //           response?.AddressBirthDetails.presentaddressCountry +
+    //           " , " +
+    //           response?.AddressBirthDetails.presentInsideKeralaPostOffice +
+    //           " , " +
+    //           response?.AddressBirthDetails.presentInsideKeralaPincode || "CR_NOT_RECORDED",
+    //     },
+
+    //     {
+    //       title: "PDF_BIRTH_PERMANENT_ADDRESS",
+    //       value:
+    //         response?.AddressBirthDetails.permntInKeralaAdrHouseNameEn +
+    //         " , " +
+    //         response?.AddressBirthDetails.permntInKeralaAdrStreetNameEn +
+    //         " , " +
+    //         response?.AddressBirthDetails.permntInKeralaAdrLocalityNameEn +
+    //         " , " +
+    //         response?.AddressBirthDetails.permntInKeralaAdrVillage +
+    //         " , " +
+    //         response?.AddressBirthDetails.permntInKeralaAdrTaluk +
+    //         " , " +
+    //         response?.AddressBirthDetails.permntInKeralaAdrDistrict +
+    //         " , " +
+    //         response?.AddressBirthDetails.permtaddressStateName +
+    //         " , " +
+    //         response?.AddressBirthDetails.permtaddressCountry +
+    //         " , " +
+    //         response?.AddressBirthDetails.permntInKeralaAdrPostOffice +
+    //         " , " +
+    //         response?.AddressBirthDetails.permntInKeralaAdrPincode,
+    //     },
+    //     {
+    //       value:
+    //         response?.AddressBirthDetails.permntInKeralaAdrHouseNameMl +
+    //         " , " +
+    //         response?.AddressBirthDetails.permntInKeralaAdrStreetNameMl +
+    //         " , " +
+    //         response?.AddressBirthDetails.permntInKeralaAdrLocalityNameMl +
+    //         " , " +
+    //         response?.AddressBirthDetails.permntInKeralaAdrVillage +
+    //         " , " +
+    //         response?.AddressBirthDetails.permntInKeralaAdrTaluk +
+    //         " , " +
+    //         response?.AddressBirthDetails.permntInKeralaAdrDistrict +
+    //         " , " +
+    //         response?.AddressBirthDetails.permtaddressStateName +
+    //         " , " +
+    //         response?.AddressBirthDetails.permtaddressCountry +
+    //         " , " +
+    //         response?.AddressBirthDetails.permntInKeralaAdrPostOffice +
+    //         " , " +
+    //         response?.AddressBirthDetails.permntInKeralaAdrPincode,
+    //     },
+    //   ],
+    // };
     const initiatorInfo = {
       title: "CR_APPLICANT_INFORMATION_HEADER",
       values: [
@@ -223,8 +341,8 @@ export const CRNACsearch = {
     response && employeeResponse.push(Birthdetails);
     response && employeeResponse.push(childdetails);
     response && employeeResponse.push(parentInfo);
-    response && employeeResponse.push(PresentAddressBirthDetailsInfo);
-    //response && employeeResponse.push(PermanentAddressBirthDetailsInfo);
+    response && employeeResponse.push(AddressBirthDetailsPresentInfo);
+    response && employeeResponse.push(AddressBirthDetailsPresentOutsideKeralaInfo);
     response && employeeResponse.push(initiatorInfo);
     // response && employeeResponse.push(childInfo);
 
