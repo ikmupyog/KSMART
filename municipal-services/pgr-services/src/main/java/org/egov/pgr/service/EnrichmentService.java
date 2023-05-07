5c090e2e03ec02347b99b0aed49d40d0fe5a2e01
vbvarsha <varsha.ikm@kerala.gov.in>
2023-05-07 17:22:59 +0530

    pgr

--- municipal-services/pgr-services/src/main/java/org/egov/pgr/service/EnrichmentService.java
+++ municipal-services/pgr-services/src/main/java/org/egov/pgr/service/EnrichmentService.java
@@ -123,15 +123,17 @@ public class EnrichmentService {
         AuditDetails auditDetails = utils.getAuditDetails(requestInfo.getUserInfo().getUuid(), service,false);
 
         service.setAuditDetails(auditDetails);     
-        System.out.println("action  "+ workflow.getAssignes().isEmpty());
-        if( !workflow.getAssignes().isEmpty()) {
-	      if(workflow.getAssignes() != null ) {
+//        System.out.println("action  "+ workflow.getAssignes().size());
+//        if( !workflow.getAssignes().isEmpty() || workflow.getAssignes() != null) {
+//	      if(workflow.getAssignes() != null ) {
+	    	  
+	     if  (workflow.getAssignes()!=null && !workflow.getAssignes().isEmpty()) {
 	    	  System.out.println("action 1 "+ workflow.getAssignes());
 	        if(workflow.getAssignes().get(0) != null) {
 	        	 service.setAccountId(workflow.getAssignes().get(0));
 	        }    
 	      }
-        }
+        
        if((workflow.getAction().equals(ACTION_REJECT)) || (workflow.getAction().equals(ACTION_RESOLVE))){
     	  System.out.println("getCreatedBy  "+service.getAuditDetails().getCreatedBy());
     	  service.setAccountId(service.getAuditDetails().getCreatedBy());
