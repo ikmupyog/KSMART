3b2c0463c0a3409327e3c30efed250d0b3c48ba4
Rakhi S <rakhis.ikm@kerala.gov.in>
2023-05-23 15:54:58 +0530

    Search - new fields added in query builder as well as rowmapper

--- municipal-services/death-services/src/main/java/org/ksmart/death/deathapplication/repository/rowmapper/DeathInitiatorRowMapper.java
+++ municipal-services/death-services/src/main/java/org/ksmart/death/deathapplication/repository/rowmapper/DeathInitiatorRowMapper.java
@@ -7,12 +7,6 @@ import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.ResultSetExtractor;
 import org.springframework.stereotype.Component;
 
-/**
-     * Creates DeathInitiatorRowMapper
-     * Jasmine
-     * on  12/02/2023,updated 15/12/2023
-     */
-    
 @Component
 public class DeathInitiatorRowMapper {
     public DeathInitiatorDtls extractData(ResultSet rs) throws SQLException, DataAccessException { 
@@ -23,7 +17,16 @@ public class DeathInitiatorRowMapper {
                             .initiatorName(rs.getString("initiator_name"))
                             .initiatorMobile(rs.getLong("initiator_mobile_no"))
                             .initiatorAddress(rs.getString("initiator_address"))
-                            .isDeclarationInitiator(rs.getBoolean("is_declaration_initiator"))                            
+                            .isDeclarationInitiator(rs.getBoolean("is_declaration_initiator"))   
+                            .initiatorDesi(rs.getString("initiator_designation"))      
+                            .initiatorInstitutionName(rs.getString("initiator_institution_name"))   
+                            .isCaretaker(rs.getBoolean("initiator_iscaretaker"))    
+                            .isGuardian(rs.getBoolean("initiator_isguardian")) 
+                            .ipopList(rs.getString("ip_op_list"))     
+                            .ipopNumber(rs.getString("ip_op_no")) 
+                            .registrationNoHospital(rs.getString("reg_no_hospital")) 
+                            .registrationNoInstitution(rs.getString("reg_no_institution")) 
+                            .admissionNoInstitution(rs.getString("admission_no_institution"))   
                             .build();
                     }
 }
