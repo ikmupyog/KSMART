ea1193e6c3abc30052fb793b88d2581fc6ccd52a
MayaDevops <unnimaya.ikm@kerala.gov.in>
2023-05-26 14:59:22 +0530

    Bug fix

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/cr/src/components/SearchApplication/SearchFields.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/cr/src/components/SearchApplication/SearchFields.js
@@ -106,7 +106,7 @@ const SearchFields = ({register, control, reset, tenantId, t,previousPage ,appli
         </SearchField>} */}
         <SearchField>
             <label>{t("CR_BIRTH_MOTHERNAME_LABEL")}</label>
-            <TextInput  name="mother" inputRef={register({})}/>
+            <TextInput  name="nameOfMother" inputRef={register({})}/>
         </SearchField>
         <SearchField className="submit">
             <SubmitBar label={t("ES_COMMON_SEARCH")} submit disabled={ applicationType?.label?false:true}/>
@@ -118,7 +118,7 @@ const SearchFields = ({register, control, reset, tenantId, t,previousPage ,appli
                     toDate: "",
                     licenseNumbers: "",
                     status: "",
-                    mother: "",
+                    nameOfMother: "",
                     offset: 0,
                     limit: 10,
                     sortBy: "dateOfBirth",
