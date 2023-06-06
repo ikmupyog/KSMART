29546d4761bce511af2b3e7b154d6a75d6bc7b01
anzarthoghtripples <anzar@thoughtripples.com>
2023-06-06 17:21:29 +0530

    dfm-module-adding

--- frontend/micro-ui/web/micro-ui-internals/packages/react-components/src/atoms/Hamburger.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/react-components/src/atoms/Hamburger.js
@@ -4,6 +4,7 @@ import { HamburgerIcon } from "./svgindex";
 const Hamburger = ({ handleClick, color }) => (
   <span style={{ marginRight: "10px" }} className="cp" onClick={handleClick}>
     <HamburgerIcon styles={{ display: "inline" }} color={color} />
+  <img src="https://ulb-logos.s3.ap-south-1.amazonaws.com/bars.svg" alt="" />
   </span>
 );
 
